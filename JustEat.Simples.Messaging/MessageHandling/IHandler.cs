a1b89052221f455c9d3627ba25498f782b59ac10
Anton <anton.jefcoate@just-eat.com>
2013-08-09 15:20:19 +0100

    XML doc for handlers

--- JustEat.Simples.Messaging/MessageHandling/IHandler.cs
+++ JustEat.Simples.Messaging/MessageHandling/IHandler.cs
@@ -1,7 +1,16 @@
 ﻿namespace JustEat.Simples.NotificationStack.Messaging.MessageHandling
 {
+    /// <summary>
+    /// Message handlers
+    /// </summary>
+    /// <typeparam name="T">Type of message to be handled</typeparam>
     public interface IHandler<in T>
     {
+        /// <summary>
+        /// Handle a message of a given type
+        /// </summary>
+        /// <param name="message">Message to handle</param>
+        /// <returns>Was handling successful?</returns>
         bool Handle(T message);
     }
 }
