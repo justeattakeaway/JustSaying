1156d6d7c27a0a840d96656804e3e7fd16934ae1
Maxim Lakomkin <maxim.lakomkin@just-eat.com>
2013-08-14 10:27:25 +0100

    Added basket and Menu APIs bits. Fixing AWSSDK references.

---
+++ JustEat.Simples.Api/Client/Menu/Models/Offer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace JustEat.Simples.Api.Client.Menu.Models
+{
+    public enum Offer
+    {
+        NoOffer = 0,
+
+        Bogof = 1,
+
+        Bogoh = 2
+    }
+}
---
+++ JustEat.Simples.Api/Client/Menu/Models/ProductDetails.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace JustEat.Simples.Api.Client.Menu.Models
+{
+    public class ProductDetails
+    {
+        public ProductDetails()
+        {
+            OptionalAccessories = new List<AccessoryDetails>();
+            RequiredAccessories = new List<AccessoryDetails>();
+            MealParts = new List<MealPartDetails>();
+        }
+
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public string Synonym { get; set; }
+        public int ProductTypeId { get; set; }
+        public bool RequireOtherProducts { get; set; }
+
+        public Offer Offer { get; set; }
+
+        public IEnumerable<AccessoryDetails> OptionalAccessories { get; set; }
+        public IEnumerable<AccessoryDetails> RequiredAccessories { get; set; }
+        public IEnumerable<MealPartDetails> MealParts { get; set; }
+
+        public bool HasMealParts { get; set; }
+    }
+}
