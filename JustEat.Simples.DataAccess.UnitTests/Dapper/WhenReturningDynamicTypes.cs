e1c8d1271f390e51d1fb8c6a755d0cd0dd44688e
Jaimal Chohan <jaimal.chohan@just-eat.com>
2013-08-19 09:26:10 +0100

    Fixed issue with calling entension methods on dynamic types.

---
+++ JustEat.Simples.DataAccess.UnitTests/Dapper/WhenReturningDynamicTypes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CSharp.RuntimeBinder;
+using NUnit.Framework;
+
+namespace JustEat.Simples.DataAccess.UnitTests.Dapper
+{
+    [TestFixture]
+    public class WhenReturningDynamicTypes
+    {
+        [Test]
+        [ExpectedException(typeof(RuntimeBinderException))]
+        public void AndCallingFirstAsExtensionMethod()
+        {
+            dynamic list = new List<int> {1, 2, 3};
+            list.First();
+        }
+
+        [Test]
+        public void AndCallingFirstFromEnumerableClass()
+        {
+            dynamic list = new List<int> { 1, 2, 3 };
+            Enumerable.First(list);
+        }
+    }
+}
