ca63892696c2322016fb205e889698fa45c45981
Anton <anton.jefcoate@just-eat.com>
2013-08-21 16:13:50 +0100

    Added legacy OrderId to order placed message. Required for mapping in lagacy view

--- JustEat.Simples.Messaging/Messages/OrderPlacement/OrderPlaced.cs
+++ JustEat.Simples.Messaging/Messages/OrderPlacement/OrderPlaced.cs
@@ -3,11 +3,13 @@ namespace JustEat.Simples.NotificationStack.Messaging.Messages.OrderPlacement
 {
     public class OrderPlaced : Message
     {
-        public OrderPlaced(Guid orderId)
+        public OrderPlaced(Guid orderId, int legacyrderId)
         {
+            LegacyrderId = legacyrderId;
             OrderId = orderId;
         }
 
         public Guid OrderId { get; private set; }
+        public int LegacyrderId { get; private set; }
     }
 }
