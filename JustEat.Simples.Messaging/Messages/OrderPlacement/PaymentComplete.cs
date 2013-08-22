f45a29e184f0ab633352be8ce20f079aa8827402
Anton <anton.jefcoate@just-eat.com>
2013-08-22 10:34:05 +0100

    Changing payment complete event to have order json rather than partial order properties

--- JustEat.Simples.Messaging/Messages/OrderPlacement/PaymentComplete.cs
+++ JustEat.Simples.Messaging/Messages/OrderPlacement/PaymentComplete.cs
@@ -2,23 +2,13 @@ namespace JustEat.Simples.NotificationStack.Messaging.Messages.OrderPlacement
 {
     public class PaymentComplete : Message
     {
-        public PaymentComplete(string orderId, bool paymentSuccessful, string lastCardDigits, string avsStuff, string paymentTransactionRef, string paymentServiceProvider, decimal totalPaid)
+        public PaymentComplete(string orderId, string compressedOrderJson)
         {
-            TotalPaid = totalPaid;
-            PaymentServiceProvider = paymentServiceProvider;
-            PaymentTransactionRef = paymentTransactionRef;
-            AvsStuff = avsStuff;
-            LastCardDigits = lastCardDigits;
-            PaymentSuccessful = paymentSuccessful;
+            CompressedOrderJson = compressedOrderJson;
             OrderId = orderId;
         }
 
         public string OrderId { get; private set; }
-        public bool PaymentSuccessful { get; private set; }
-        public string LastCardDigits { get; private set; }
-        public string AvsStuff { get; private set; }
-        public string PaymentTransactionRef { get; private set; }
-        public string PaymentServiceProvider { get; private set; }
-        public decimal TotalPaid { get; private set; }
+        public string CompressedOrderJson { get; private set; }
     }
 }
