96c226c519a6a9435a2db02104e59f26d825a521
Anton <anton.jefcoate@just-eat.com>
2013-08-22 12:04:54 +0100

    Decided against passing entire order json with message

--- JustEat.Simples.Messaging/Messages/OrderPlacement/PaymentComplete.cs
+++ JustEat.Simples.Messaging/Messages/OrderPlacement/PaymentComplete.cs
@@ -2,13 +2,11 @@ namespace JustEat.Simples.NotificationStack.Messaging.Messages.OrderPlacement
 {
     public class PaymentComplete : Message
     {
-        public PaymentComplete(string orderId, string compressedOrderJson)
+        public PaymentComplete(string orderId)
         {
-            CompressedOrderJson = compressedOrderJson;
             OrderId = orderId;
         }
 
         public string OrderId { get; private set; }
-        public string CompressedOrderJson { get; private set; }
     }
 }
