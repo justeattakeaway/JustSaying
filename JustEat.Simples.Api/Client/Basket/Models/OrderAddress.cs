5f01faf7e44defcd17adfca15c6f22c6ade40da0
Anton <anton.jefcoate@just-eat.com>
2013-08-27 12:29:36 +0100

    added missing field

--- JustEat.Simples.Api/Client/Basket/Models/OrderAddress.cs
+++ JustEat.Simples.Api/Client/Basket/Models/OrderAddress.cs
@@ -2,6 +2,7 @@ namespace JustEat.Simples.Api.Client.Basket.Models
 {
     public class OrderAddress
     {
+        public string Name { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string Email { get; set; }
