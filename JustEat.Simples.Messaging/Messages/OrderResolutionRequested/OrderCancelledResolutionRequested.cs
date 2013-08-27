4bbf4ab341a17015b05f9d3eb3e193ab98c56081
Jaimal Chohan <jaimal.chohan@just-eat.com>
2013-08-27 15:07:35 +0100

    SIM-773: New optional constructor paramater defaulted to false.

--- JustEat.Simples.Messaging/Messages/OrderResolutionRequested/OrderCancelledResolutionRequested.cs
+++ JustEat.Simples.Messaging/Messages/OrderResolutionRequested/OrderCancelledResolutionRequested.cs
@@ -2,10 +2,13 @@ namespace JustEat.Simples.NotificationStack.Messaging.Messages.OrderResolutionRe
 {
     public class OrderCancelledResolutionRequested : OrderResolutionRequestedMessage
     {
-        public OrderCancelledResolutionRequested(int orderId, string auditComment, bool notifiyCustomer, OrderResolutionStatus orderResolutionStatus)
-            : base(orderId, auditComment, notifiyCustomer, orderResolutionStatus)
-        {
-            
-        }
+        public OrderCancelledResolutionRequested(int orderId, string auditComment, bool notifiyCustomer,
+                                                 OrderResolutionStatus orderResolutionStatus)
+            : base(orderId, auditComment, notifiyCustomer, orderResolutionStatus) {}
+
+        public OrderCancelledResolutionRequested(int orderId, string auditComment, bool notifiyCustomer,
+                                                 OrderResolutionStatus orderResolutionStatus,
+                                                 bool removeFromUnverifiedOrders)
+            : base(orderId, auditComment, notifiyCustomer, orderResolutionStatus, removeFromUnverifiedOrders) {}
     }
 }
