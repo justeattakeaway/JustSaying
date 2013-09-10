15b780e4cd871f88aa5ea25fde082a525bd7dc5f
Anton <anton.jefcoate@just-eat.com>
2013-09-10 11:11:30 +0100

    Correct oopsie

--- JustEat.Simples.Api/Client/Restaurant/Models/RestaurantDetail.cs
+++ JustEat.Simples.Api/Client/Restaurant/Models/RestaurantDetail.cs
@@ -5,12 +5,11 @@ namespace JustEat.Simples.Api.Client.Restaurant.Models
     public class RestaurantDetail
     {
         public int RestaurantId { get; set; }
-
         public string Name { get; set; }
-
         public string PhoneNumber { get; set; }
-
         public int RestaurantType { get; set; }
+        public string SeoName { get; set; }
+        public string LogoUrl { get; set; }
     }
 
     public class OperationalStatus
