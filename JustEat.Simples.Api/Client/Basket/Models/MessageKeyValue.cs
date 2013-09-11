171be4081053db6ef4c649f5bdf11ae25a2cf9ea
Anton <anton.jefcoate@just-eat.com>
2013-09-11 10:12:17 +0100

    Clean solution of unwanted projects

--- JustEat.Simples.Api/Client/Basket/Models/MessageKeyValue.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JustEat.Simples.Api.Client.Basket.Models
-{
-    public class MessageKeyValue
-    {
-        public string KeyName { get; set; }
-        public string DisplayValue { get; set; }
-    }
-}
