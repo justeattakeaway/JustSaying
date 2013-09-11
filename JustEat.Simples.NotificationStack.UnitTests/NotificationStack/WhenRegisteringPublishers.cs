e9600d3bf2083d01a4b66ac887619b1ca0a7ba72
Anton <anton.jefcoate@just-eat.com>
2013-09-11 11:25:40 +0100

    Missed file

--- JustEat.Simples.NotificationStack.UnitTests/NotificationStack/WhenRegisteringPublishers.cs
+++ JustEat.Simples.NotificationStack.UnitTests/NotificationStack/WhenRegisteringPublishers.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Threading;
 using JustEat.Simples.NotificationStack.Messaging;
-using JustEat.Simples.NotificationStack.Messaging.Messages;
 using JustEat.Simples.NotificationStack.Messaging.Messages.OrderDispatch;
 using JustEat.Testing;
 using NSubstitute;
@@ -39,34 +37,6 @@ namespace Stack.UnitTests.NotificationStack
         public void RejectedOrderWasPublishedTwice()
         {
             _publisher.Received(2).Publish(Arg.Any<OrderRejected>());
-        }
-    }
-
-    public class WhenPublishingFails : NotificationStackBaseTest
-    {
-        private readonly IMessagePublisher _publisher = Substitute.For<IMessagePublisher>();
-        private const int PublishAttempts = 4;
-
-        protected override void Given()
-        {
-            Config.PublishFailureReAttempts.Returns(4);
-            Config.PublishFailureBackoffMilliseconds.Returns(1);
-            RecordAnyExceptionsThrown();
-            _publisher.When(x => x.Publish(Arg.Any<Message>())).Do(x => { throw new Exception(); });
-        }
-
-        protected override void When()
-        {
-            SystemUnderTest.AddMessagePublisher<OrderAccepted>("OrderDispatch", _publisher);
-
-            SystemUnderTest.Publish(new OrderAccepted(0, 0, 0));
-            Thread.Sleep(20);
-        }
-
-        [Then]
-        public void EventPublicationWasAttemptedTheConfiguredNumberOfTimes()
-        {
-            _publisher.Received(PublishAttempts).Publish(Arg.Any<OrderAccepted>());
         }
     }
 }
