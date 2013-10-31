ac7b366a49b9b3aa0df04883b2d8c83524a12cc8
Payman Labbaf <payman.labbaf@just-eat.com>
2013-10-31 11:31:00 +0000

    Adding ability to create Dynamo at runtime.

--- JustEat.Simples.AwsTools.UnitTests/DynamoDbStoreIntregrationTests.cs
+++ JustEat.Simples.AwsTools.UnitTests/DynamoDbStoreIntregrationTests.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Amazon;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
 using JustEat.Simples.NotificationStack.AwsTools;
 using NUnit.Framework;
 
@@ -24,6 +28,96 @@ namespace AwsTools.UnitTests
             var footprint = provider.Read<MessageFootprint>(key, defaultDynamoDbOperationConfig);
             Assert.IsNotNull(footprint);
             Console.WriteLine(footprint.MessageId);
+        }
+    }
+
+    [TestFixture]
+    public class DynamoDbTests
+    {
+        private string _tableName;
+        private AmazonDynamoDBClient _client;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _client = new AmazonDynamoDBClient(RegionEndpoint.EUWest1);
+            _tableName = "Test-" + Guid.NewGuid();
+        }
+
+        [Test]
+        public void CreateDynamoDbIfDoesNotExist()
+        {
+            DynamoDbConfig config = GetDynamoDbConfig();
+
+            DynamoDb db = new DynamoDb(config, _client);
+
+            AssertDynamoDbExists(_tableName);
+        }
+
+        [Test]
+        public void CanHandleConcurrentCalls()
+        {
+            DynamoDbConfig config = GetDynamoDbConfig();
+
+            Task.Factory.StartNew(() => new DynamoDb(config, _client));
+            var tasks = Enumerable.Repeat(Task.Factory.StartNew(() => { new DynamoDb(config, _client); }), 10);
+            Task.WaitAll(tasks.ToArray());
+
+            AssertDynamoDbExists(_tableName);
+        }
+
+        private DynamoDbConfig GetDynamoDbConfig()
+        {
+            return new DynamoDbConfig
+                {
+                    TableName = _tableName,
+                    AttributeDefinitions = new List<AttributeDefinition>()
+                        {
+                            new AttributeDefinition
+                                {
+                                    AttributeName = "Id",
+                                    AttributeType = "N"
+                                }
+                        },
+                    KeySchema = new List<KeySchemaElement>
+                        {
+                            new KeySchemaElement
+                                {
+                                    AttributeName = "Id",
+                                    KeyType = "HASH"
+                                }
+                        },
+                    ProvisionedThroughput = new ProvisionedThroughput
+                        {
+                            ReadCapacityUnits = 1,
+                            WriteCapacityUnits = 1
+                        }
+                };
+        }
+
+        private void AssertDynamoDbExists(string tableName)
+        {
+            var request = new DescribeTableRequest { TableName = tableName };
+            try
+            {
+                var response = _client.DescribeTable(request);
+            }
+            catch (ResourceNotFoundException)
+            {
+
+                Assert.Fail("Table {0} doesn't exist!", _tableName);
+            }
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            try
+            {
+                var request = new DeleteTableRequest { TableName = _tableName };
+                var response = _client.DeleteTable(request);
+            }
+            catch { }
         }
     }
 }
---
+++ JustEat.Simples.AwsTools/DynamoDb.cs
@@ -0,0 +1,86 @@
+﻿using System.Linq;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using NLog;
+
+namespace JustEat.Simples.NotificationStack.AwsTools
+{
+    public class DynamoDb
+    {
+        private readonly DynamoDbConfig _config;
+        private readonly AmazonDynamoDBClient _client;
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        private readonly string[] _expectedErrorCodesForConcurrentCalls = new[] { "ResourceInUseException", "ThrottlingException" };
+
+        public DynamoDb(DynamoDbConfig config, AmazonDynamoDBClient client)
+        {
+            _config = config;
+            _client = client;
+            CreateTableIfNotExist();
+        }
+
+        private void CreateTableIfNotExist()
+        {
+            var request = new DescribeTableRequest { TableName = _config.TableName };
+            try
+            {
+                _client.DescribeTable(request);
+            }
+            catch (ResourceNotFoundException)
+            {
+                CreateTable();
+            }
+        }
+
+        private void CreateTable()
+        {
+            var request = new CreateTableRequest
+                {
+                    AttributeDefinitions = _config.AttributeDefinitions,
+                    KeySchema = _config.KeySchema,
+                    ProvisionedThroughput = _config.ProvisionedThroughput,
+                    TableName = _config.TableName
+                };
+            try
+            {
+                var response = _client.CreateTable(request);
+                WaitUntilTableReady(_config.TableName);
+            }
+            catch (AmazonDynamoDBException ex)
+            {
+                if (!_expectedErrorCodesForConcurrentCalls.Contains(ex.ErrorCode))
+                {
+                    throw;
+                }
+            }
+
+
+        }
+        private void WaitUntilTableReady(string tableName)
+        {
+            string status = null;
+            // Let us wait until table is created. Call DescribeTable.
+            do
+            {
+                System.Threading.Thread.Sleep(5000);
+                try
+                {
+                    var res = _client.DescribeTable(new DescribeTableRequest
+                        {
+                            TableName = tableName
+                        });
+
+                    Log.Info("Dynamo Table name: {0}, status: {1}",
+                                      res.DescribeTableResult.Table.TableName,
+                                      res.DescribeTableResult.Table.TableStatus);
+                    status = res.DescribeTableResult.Table.TableStatus;
+                }
+                catch (ResourceNotFoundException resourceNotFound)
+                {
+                    // DescribeTable is eventually consistent. So you might
+                    // get resource not found. So we handle the potential exception.
+                }
+            } while (status != "ACTIVE");
+        }
+    }
+}
