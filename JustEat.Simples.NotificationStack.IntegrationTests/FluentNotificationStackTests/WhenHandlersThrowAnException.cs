4a1ad6cdc89d7fb75d930c240168032b73a917cc
Payman Labbaf <payman.labbaf@just-eat.com>
2014-02-25 20:23:42 +0000

    Removed awkward and impossible test.

--- JustEat.Simples.NotificationStack.IntegrationTests/FluentNotificationStackTests/WhenHandlersThrowAnException.cs
+++ JustEat.Simples.NotificationStack.IntegrationTests/FluentNotificationStackTests/WhenHandlersThrowAnException.cs
@@ -20,11 +20,9 @@ namespace NotificationStack.IntegrationTests.FluentNotificationStackTests
 
         protected override void Given()
         {
-            _component = "test" + DateTime.Now.Ticks;
             RecordAnyExceptionsThrown();
             base.Given();
             RegisterHandler(_handler);
-            RegisterConfig(new MessagingConfig { Component = _component, Tenant = "uk", Environment = "int", PublishFailureBackoffMilliseconds = 1, PublishFailureReAttempts = 3 });
         }
 
         protected override void When()
@@ -32,52 +30,12 @@ namespace NotificationStack.IntegrationTests.FluentNotificationStackTests
             ServiceBus.Publish(new GenericMessage());
         }
 
-        [Then, Explicit]
+        [Then]
         public void ThenExceptionIsRecordedInStatsD()
         {
             _handler.WaitUntilCompletion(10.Seconds()).ShouldBeTrue();
             Thread.Sleep(1000);
             Monitoring.Received().HandleException(Arg.Any<string>());
-        }
-
-        [Then]
-        public void EventuallyTheMessageEndsUpInErrorQueue()
-        {
-            _handler.WaitUntilCompletion(10.Seconds());
-            _queue = new SqsQueueByName(string.Format("uk-int-{0}-1-customercommunication", _component), AWSClientFactory.CreateAmazonSQSClient(RegionEndpoint.EUWest1));
-            _queue.Exists();
-
-            var errorQueue = _queue.ErrorQueue;
-
-            PatientlyAssert(() =>
-            {
-                errorQueue.Exists();
-                var sqsMessageResponse =
-                    errorQueue.Client.ReceiveMessage(new ReceiveMessageRequest
-                    {
-                        QueueUrl = errorQueue.Url,
-                        MaxNumberOfMessages = 1,
-                        WaitTimeSeconds = 1
-                    });
-               return sqsMessageResponse.Messages.Any();
-            });
-        }
-
-        public void PatientlyAssert(Func<bool> expression)
-        {
-            bool result = false;
-            bool timedOut;
-            var started = DateTime.Now;
-            do
-            {
-                try { result = expression.Invoke(); }
-                catch { }
-                timedOut = TimeSpan.FromSeconds(20) < DateTime.Now - started;
-                Thread.Sleep(TimeSpan.FromSeconds(5));
-                Console.WriteLine("{0} - Still Checking...", (DateTime.Now - started).TotalSeconds);
-            } while (!result && !timedOut);
-
-            Assert.True(result);
         }
 
         public override void PostAssertTeardown()
