2deafeff0f9d7af66096693f1d4b39d028a1573f
Anton <anton.jefcoate@just-eat.com>
2014-03-18 11:43:54 +0000

    remove unused locator

--- JustEat.Simples.NotificationStack/Lookups/IPublishEndpointProvider.cs
+++ JustEat.Simples.NotificationStack/Lookups/IPublishEndpointProvider.cs
@@ -5,7 +5,6 @@ namespace JustEat.Simples.NotificationStack.Stack.Lookups
 {
     public interface IPublishEndpointProvider
     {
-        string GetLocationEndpoint(string location);
         string GetLocationName(string location);
     }
 
@@ -16,22 +15,6 @@ namespace JustEat.Simples.NotificationStack.Stack.Lookups
         public SnsPublishEndpointProvider(IMessagingConfig config)
         {
             _config = config;
-        }
-
-        public string GetLocationEndpoint(string location)
-        {
-            throw new NotImplementedException("Not implemented yet. Come back later");
-            //// Eventually this should come from the Settings API (having been published somewhere by the create process).
-            //switch (location)
-            //{
-            //    case NotificationTopic.OrderDispatch:
-            //        return "arn:aws:sns:eu-west-1:507204202721:uk-qa12-order-dispatch";
-
-            //    case NotificationTopic.CustomerCommunication:
-            //        return "arn:aws:sns:eu-west-1:507204202721:uk-qa12-customer-order-communication";
-            //} 
-
-            //throw new IndexOutOfRangeException("There is no endpoint defined for the provided location type");
         }
 
         public string GetLocationName(string location)
