915a1ca9ffce7dc9dd766c0579a6f3513039bc17
Payman Labbaf <payman.labbaf@just-eat.com>
2014-04-05 13:24:27 +0100

    JustSaying will always require region. Default eu-west-1 region is only applied to JustEat extensions.

--- JustEat.Simples.NotificationStack/CreateMe.cs
+++ JustEat.Simples.NotificationStack/CreateMe.cs
@@ -1,19 +1,27 @@
 using System;
+using Amazon;
 using JustSaying;
 using JustSaying.AwsTools.QueueCreation;
 using JustSaying.Messaging.MessageSerialisation;
 using JustSaying.Stack;
+using NLog;
 using MessagingConfig = JustSaying.Stack.MessagingConfig;
 
 namespace JustSayingExtensions
 {
     public static class CreateMe
     {
+        private static readonly Logger Log = LogManager.GetLogger("JustSaying"); // ToDo: Dangerous!
+
         public static IFluentMonitoring AJustEatBus(Action<INotificationStackConfiguration> configuration)
         {
             var config = new MessagingConfig();
             configuration.Invoke(config);
-
+            if (string.IsNullOrWhiteSpace(config.Region))
+            {
+                config.Region = RegionEndpoint.EUWest1.SystemName; // ToDo: Why is this in the base impl?
+                Log.Info("No Region was specified, using {0} by default.", config.Region);
+            }
             config.Validate();
 
             return new FluentNotificationStack(new JustSayingBus(config, new MessageSerialisationRegister()), new AmazonQueueCreator());
--- JustSaying/CreateMe.cs
+++ JustSaying/CreateMe.cs
@@ -11,19 +11,11 @@ namespace JustSaying
     /// </summary>
     public static class CreateMe
     {
-        private static readonly Logger Log = LogManager.GetLogger("JustSaying"); // ToDo: Dangerous!
-
         public static IAmJustSayingFluently ABus(Action<IPublishConfiguration> configuration)
         {
             var config = new MessagingConfig();
             configuration.Invoke(config);
             config.Validate();
-
-            if (string.IsNullOrWhiteSpace(config.Region))
-            {
-                config.Region = RegionEndpoint.EUWest1.SystemName; // ToDo: Why is this in the base impl?
-                Log.Info("No Region was specified, using {0} by default.", config.Region);
-            }
 
             return new JustSayingFluently(new JustSayingBus(config, new MessageSerialisationRegister()), new AmazonQueueCreator());
         }
