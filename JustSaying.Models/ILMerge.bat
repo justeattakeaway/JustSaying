6ab215c789a2473daadc6f8f8873ed72fdb7cec0
Jaimal Chohan <jaimalchohan@gmail.com>
2014-06-24 16:53:15 +0100

    Rename JustSaying,Models to JustSaying.Models and add ILmerge and nuspec files so we have nice packages

---
+++ JustSaying.Models/ILMerge.bat
@@ -0,0 +1,3 @@
+if not exist merged md merged
+
+"..\packages\ilmerge.2.13.0307\ILMerge.exe" /out:merged\JustSaying.Models.dll /internalize /targetplatform:v4 "bin\Release\JustSaying.Models.dll" "bin\Release\Newtonsoft.Json.dll"
