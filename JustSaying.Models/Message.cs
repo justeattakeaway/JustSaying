311b0634066926527cd5f8e29fc7611ca14784e8
Jaimal Chohan <jaimalchohan@gmail.com>
2014-06-26 10:23:36 +0100

    Removed the dependecy on JSON.net from Models as NullHandling.Ignore is now global to ensure backwards compatability for all properties

--- JustSaying.Models/Message.cs
+++ JustSaying.Models/Message.cs
@@ -1,5 +1,4 @@
 using System;
-using Newtonsoft.Json;
 
 namespace JustSaying.Models
 {
@@ -16,8 +15,6 @@ namespace JustSaying.Models
         public string Version{ get; private set; }
         public string SourceIp { get; private set; }
         public string Tenant { get; set; }
-
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Conversation { get; set; }
         
         //footprint in order to avoid the same message being processed multiple times.
