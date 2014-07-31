a111346b764d1be4e6f23a10cc42354a51b26e0b
dave.marsland <dave.marsland@just-eat.com>
2014-07-31 15:21:59 +0100

    Correct casing for JUST EAT plc

--- JustSaying.Messaging.UnitTests/Properties/AssemblyInfo.cs
+++ JustSaying.Messaging.UnitTests/Properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("JustSaying.Messaging.UnitTests")]
 [assembly: AssemblyDescription("JustSaying is a light-weight service bus on top of AWS services that allows communication via messaging in a distributed architecture.")]
-[assembly: AssemblyCompany("JUST EAT PLC")]
-[assembly: AssemblyCopyright("Copyright © JUST EAT PLC 2013")]
+[assembly: AssemblyCompany("JUST EAT plc")]
+[assembly: AssemblyCopyright("Copyright © JUST EAT plc 2013")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("0.0.0")]
 
