a82c16834061e15480a0c41a2bcd6e7c3731ba3b
Anton <anton.jefcoate@just-eat.com>
2014-09-03 17:10:49 +0100

    Oops, misses save

--- JustSaying.AwsTools.UnitTests/Sqs/WhenFetchingQueueByName.cs
+++ JustSaying.AwsTools.UnitTests/Sqs/WhenFetchingQueueByName.cs
@@ -2,8 +2,6 @@
 using Amazon.SQS;
 using Amazon.SQS.Model;
 using JustBehave;
-using JustSaying.Messaging.MessageSerialisation;
-using JustSaying.TestingFramework;
 using NSubstitute;
 using NUnit.Framework;
 
@@ -47,54 +45,6 @@ namespace JustSaying.AwsTools.UnitTests.Sqs
         {
             var sqsQueueByName = new SqsQueueByName("some-queue-name", _client, RetryCount);
             Assert.IsTrue(sqsQueueByName.Exists());
-        }
-    }
-
-    public class WhenPublishing : BehaviourTest<SqsPublisher>
-    {
-        private readonly IMessageSerialisationRegister _serialisationRegister = Substitute.For<IMessageSerialisationRegister>();
-        private readonly IAmazonSQS _sqs = Substitute.For<IAmazonSQS>();
-        private const string Url = "https://blablabla/" + QueueName;
-        private readonly GenericMessage _message = new GenericMessage();
-        private const string QueueName = "queuename";
-
-        protected override SqsPublisher CreateSystemUnderTest()
-        {
-            return new SqsPublisher(QueueName, _sqs, 0, _serialisationRegister);
-        }
-
-        protected override void Given()
-        {
-
-            var serialiser = Substitute.For<IMessageSerialiser<GenericMessage>>();
-            _serialisationRegister.GetSerialiser(typeof(GenericMessage)).Returns(serialiser);
-            _sqs.ListQueues(Arg.Any<ListQueuesRequest>()).Returns(new ListQueuesResponse{QueueUrls = new List<string>{Url}});
-            _sqs.GetQueueAttributes(Arg.Any<GetQueueAttributesRequest>()).Returns(new GetQueueAttributesResponse());
-        }
-
-        protected override void When()
-        {
-            SystemUnderTest.Publish(_message);
-        }
-
-        [Then]
-        public void MessageIsPublishedToQueue()
-        {
-            // ToDo: Can be better...
-            _sqs.Received().SendMessage(Arg.Is<SendMessageRequest>(x => x.MessageBody.Contains("\"Message\":{\"Id\":\"" + _message.Id)));
-        }
-
-        [Then]
-        public void MessageSubjectIsObjectType()
-        {
-            // ToDo: Can be better...
-            _sqs.Received().SendMessage(Arg.Is<SendMessageRequest>(x => x.MessageBody.Contains("\"Subject\":\"GenericMessage\"")));
-        }
-
-        [Then]
-        public void MessageIsPublishedToCorrectLocation()
-        {
-            _sqs.Received().SendMessage(Arg.Is<SendMessageRequest>(x => x.QueueUrl == Url));
         }
     }
 }
