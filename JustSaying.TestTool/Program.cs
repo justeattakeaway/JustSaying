794f8706b455f7d303f8283d4915281a49b4f936
brainmurphy <brianmurphyyy@gmail.com>
2014-11-26 17:05:25 +0000

    Little test tool to pub/sub.
    pub:
    JustSaying.TestTool.exe publisher eu-west-1
    
    sub:
    JustSaying.TestTool.exe subscriber

---
+++ JustSaying.TestTool/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Amazon;
+using JustSaying.Messaging.MessageHandling;
+using JustSaying.Models;
+
+namespace JustSaying.TestTool
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args[0] == "publisher")
+            {
+                Publisher(args[1]);
+            }
+            else if (args[0] == "subscriber")
+            {
+                Subscriber();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Publisher(string region)
+        {
+            CreateMeABus
+                .InRegion(region)
+                .WithSqsMessagePublisher<TestMessage>(configuration => { })
+                .Publish(new TestMessage {Text = string.Format("Hello from {0}", region)});
+        }
+
+        private static void Subscriber()
+        {
+            var handler = new TestMessageHandler();
+
+            CreateMeABus
+                .InRegion(RegionEndpoint.EUWest1.SystemName)
+                .WithFailoverRegion("us-east-1")
+                .WithSqsPointToPointSubscriber()
+                .IntoQueue(string.Empty)
+                .ConfigureSubscriptionWith(configuration => { })
+                .WithMessageHandler(handler)
+                .StartListening();
+        }
+    }
+
+    public class TestMessageHandler : IHandler<TestMessage>
+    {
+        public bool Handle(TestMessage message)
+        {
+            Console.WriteLine(message.Text);
+            return true;
+        }
+    }
+
+    public class TestMessage : Message
+    {
+        public string Text { get; set; }
+    }
+}
