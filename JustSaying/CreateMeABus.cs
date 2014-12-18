ef121b93f19af93d2be3894ff80dd75a428bd5e6
Ed Courtenay <ed@edcourtenay.co.uk>
2014-12-18 12:22:04 +0000

    Removed redundant constructor and field member from CreateMeABus; renamed file to match class name

--- JustSaying/CreateMeABus.cs
+++ JustSaying/CreateMeABus.cs
@@ -1,19 +1,11 @@
-using System;
 using JustSaying.AwsTools.QueueCreation;
 using JustSaying.Messaging.MessageSerialisation;
 using JustSaying.Messaging.Monitoring;
 
 namespace JustSaying
 {
-    public class CreateMeABus
+    public static class CreateMeABus
     {
-        private readonly MessagingConfig _config;
-
-        public CreateMeABus(MessagingConfig config)
-        {
-            _config = config;
-        }
-
         public static IMayWantOptionalSettings InRegion(string region)
         {
             var config = new MessagingConfig {Region = region};
