578625c2d9f589ed83c507a31915d922d481c2ed
Jaimal Chohan <jaimal.chohan@just-eat.com>
2015-01-10 20:36:40 +0000

    Remove redundant references and code after implementing FindTopic from the SDK

--- JustSaying.AwsTools/SnsTopicByName.cs
+++ JustSaying.AwsTools/SnsTopicByName.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Linq;
 using Amazon.SimpleNotificationService;
 using Amazon.SimpleNotificationService.Model;
-using JustSaying.Messaging;
 using JustSaying.Messaging.MessageSerialisation;
 using NLog;
 
@@ -10,14 +7,12 @@ namespace JustSaying.AwsTools
 {
     public class SnsTopicByName : SnsTopicBase
     {
-        private readonly IMessageSerialisationRegister _serialisationRegister;
         public string TopicName { get; private set; }
         private static readonly Logger Log = LogManager.GetLogger("JustSaying");
 
         public SnsTopicByName(string topicName, IAmazonSimpleNotificationService client, IMessageSerialisationRegister serialisationRegister)
             : base(serialisationRegister)
         {
-            _serialisationRegister = serialisationRegister;
             TopicName = topicName;
             Client = client;
             Exists();
@@ -48,11 +43,5 @@ namespace JustSaying.AwsTools
             Log.Info(string.Format("Failed to create Topic: {0}", TopicName));
             return false;
         }
-
-        public void Delete()
-        {
-            //No need to delete a topic ever.
-        }
-        
     }
 }
