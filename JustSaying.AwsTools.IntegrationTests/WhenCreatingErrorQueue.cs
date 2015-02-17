068c83ba72908ff41532621e1a115e0146eec847
Payman Labbaf <payman.labbaf@gmail.com>
2015-02-17 09:02:21 +0000

    Issue #57 - Ensure error queue retention period is saved and maintained properly.

---
+++ JustSaying.AwsTools.IntegrationTests/WhenCreatingErrorQueue.cs
@@ -0,0 +1,40 @@
+using System;
+using Amazon;
+using JustBehave;
+using JustSaying.AwsTools.QueueCreation;
+using NUnit.Framework;
+
+namespace JustSaying.AwsTools.IntegrationTests
+{
+    public  class WhenCreatingErrorQueue : BehaviourTest<ErrorQueue>
+    {
+        protected string QueueUniqueKey;
+
+        protected override void Given()
+        { }
+        protected override void When()
+        {
+
+            SystemUnderTest.Create(new SqsBasicConfiguration { ErrorQueueRetentionPeriodSeconds = JustSayingConstants.MAXIMUM_RETENTION_PERIOD, ErrorQueueOptOut = true});
+
+            SystemUnderTest.UpdateQueueAttribute(new SqsBasicConfiguration { ErrorQueueRetentionPeriodSeconds = 100 });
+        }
+
+        protected override ErrorQueue CreateSystemUnderTest()
+        {
+            QueueUniqueKey = "test" + DateTime.Now.Ticks;
+            return new ErrorQueue(QueueUniqueKey, AWSClientFactory.CreateAmazonSQSClient(RegionEndpoint.EUWest1));
+        }
+        public override void PostAssertTeardown()
+        {
+            SystemUnderTest.Delete();
+            base.PostAssertTeardown();
+        }
+
+        [Test]
+        public void TheRetentionPeriodOfTheErrorQueueStaysAsMaximum()
+        {
+            Assert.AreEqual(100, SystemUnderTest.MessageRetentionPeriod);
+        }
+    }
+}
--- JustSaying.AwsTools/ErrorQueue.cs
+++ JustSaying.AwsTools/ErrorQueue.cs
@@ -1,14 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Net;
+using System.Threading;
 using Amazon.SQS;
+using Amazon.SQS.Model;
 using Amazon.SQS.Util;
 using JustSaying.AwsTools.QueueCreation;
+using NLog;
 
 namespace JustSaying.AwsTools
 {
     public class ErrorQueue : SqsQueueByNameBase
     {
+        private static readonly Logger Log = LogManager.GetLogger("JustSaying");
         public ErrorQueue(string sourceQueueName, IAmazonSQS client)
             : base(sourceQueueName + "_error", client)
         {
@@ -29,7 +34,75 @@ namespace JustSaying.AwsTools
             if (!queueConfig.ErrorQueueOptOut)
                 throw new InvalidOperationException("Cannot create a dead letter queue for a dead letter queue.");
 
-            return base.Create(queueConfig, attempt: attempt);
+            try
+            {
+                var result = Client.CreateQueue(new CreateQueueRequest
+                {
+                    QueueName = QueueName,
+                    Attributes = GetCreateQueueAttributes(queueConfig.ErrorQueueRetentionPeriodSeconds, JustSayingConstants.DEFAULT_VISIBILITY_TIMEOUT)
+                });
+
+                if (!string.IsNullOrWhiteSpace(result.QueueUrl))
+                {
+                    Url = result.QueueUrl;
+                    SetQueueProperties();
+
+                    Log.Info(string.Format("Created Queue: {0} on Arn: {1}", QueueName, Arn));
+                    return true;
+                }
+            }
+            catch (AmazonSQSException ex)
+            {
+                if (ex.ErrorCode == "AWS.SimpleQueueService.QueueDeletedRecently")
+                {
+                    // Ensure we wait for queue delete timeout to expire.
+                    Log.Info(string.Format("Waiting to create Queue due to AWS time restriction - Queue: {0}, AttemptCount: {1}", QueueName, attempt + 1));
+                    Thread.Sleep(60000);
+                    Create(queueConfig, attempt: attempt++);
+                }
+                else
+                {
+                    // Throw all errors which are not delete timeout related.
+                    Log.ErrorException(string.Format("Create Queue error: {0}", QueueName), ex);
+                    throw;
+                }
+
+                // If we're on a delete timeout, throw after 2 attempts.
+                if (attempt >= 2)
+                {
+                    Log.ErrorException(string.Format("Create Queue error, max retries exceeded for delay - Queue: {0}", QueueName), ex);
+                    throw;
+                }
+            }
+
+            Log.Info(string.Format("Failed to create Queue: {0}", QueueName));
+            return false;
+
+        }
+
+        protected internal override void UpdateQueueAttribute(SqsBasicConfiguration queueConfig)
+        {
+            if (QueueNeedsUpdating(queueConfig))
+            {
+                var response = Client.SetQueueAttributes(
+                    new SetQueueAttributesRequest
+                    {
+                        QueueUrl = Url,
+                        Attributes = new Dictionary<string, string>
+                        {
+                            {JustSayingConstants.ATTRIBUTE_RETENTION_PERIOD, queueConfig.ErrorQueueRetentionPeriodSeconds.ToString()},
+                        }
+                    });
+                if (response.HttpStatusCode == HttpStatusCode.OK)
+                {
+                    MessageRetentionPeriod = queueConfig.ErrorQueueRetentionPeriodSeconds;
+                }
+            }
+        }
+
+        protected override bool QueueNeedsUpdating(SqsBasicConfiguration queueConfig)
+        {
+            return MessageRetentionPeriod != queueConfig.ErrorQueueRetentionPeriodSeconds;
         }
     }
 }
