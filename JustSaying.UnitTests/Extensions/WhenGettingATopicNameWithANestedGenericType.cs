abf1272cb96a5dd41d3ee3a6c8dd65a97050c4e7
Sam Whale <samwhale@hotmail.com>
2015-07-02 16:10:53 +0100

    Refactoring

---
+++ JustSaying.UnitTests/Extensions/WhenGettingATopicNameWithANestedGenericType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace JustSaying.UnitTests.Extensions
+{
+    public class WhenGettingATopicNameWithANestedGenericType : GivenIHaveAMessageType
+    {
+        protected override Type CreateSystemUnderTest()
+        {
+            return typeof(GenericClass<GenericClass<Poco>>);
+        }
+
+        [Test]
+        public void TheTopicNameComesTypesFromAllTypes()
+        {
+            Assert.That(Result, Is.EqualTo("genericclass_1-genericclass_1-poco"));
+        }
+    }
+}
--- JustSaying/Extensions/TypeExtensions.cs
+++ JustSaying/Extensions/TypeExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -10,22 +11,39 @@ namespace JustSaying.Extensions
 
         public static string ToTopicName(this Type type)
         {
-            var name = type.IsGenericType
-                ? Regex.Replace(type.FullName, "\\W", "_").ToLower()
-                : type.Name.ToLower();
-
-            if (name.Length > MAX_TOPIC_NAME_LENGTH)
-            {
-                var suffix = name.GetInvariantHashCode().ToString();
-                name = name.Substring(0, MAX_TOPIC_NAME_LENGTH - suffix.Length) + suffix;
-            }
-
+            var name = type.GetTopicName().TruncateTo(MAX_TOPIC_NAME_LENGTH);
             return name;
         }
 
         private static int GetInvariantHashCode(this string value)
         {
             return value.Aggregate(5381, (current, character) => (current * 397) ^ character);
+        }
+
+        private static string GetTopicName(this Type type)
+        {
+            var list = new List<string>();
+            RecursiveGetTopicName(type, list);
+            return string.Join("-", list);
+        }
+
+        private static string TruncateTo(this string name, int maxLength)
+        {
+            if (name.Length <= maxLength) return name;
+
+            var suffix = name.GetInvariantHashCode().ToString();
+            name = name.Substring(0, maxLength - suffix.Length) + suffix;
+
+            return name;
+        }
+
+        private static void RecursiveGetTopicName(Type type, ICollection<string> types)
+        {
+            types.Add(Regex.Replace(type.Name, "\\W", "_").ToLower());
+            foreach (var innerType in type.GetGenericArguments())
+            {
+                RecursiveGetTopicName(innerType, types);
+            }
         }
     }
 }
