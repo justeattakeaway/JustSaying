8b96a8513e498976e085dffa929191731433a784
Darius Damalakas <darius.damalakas@gmail.com>
2016-01-15 09:30:38 +0000

    New AWS client factory to be used in CI mode

---
+++ JustSaying.TestingFramework/IntegrationAwsClientFactory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Amazon;
+using Amazon.Runtime;
+using Amazon.SimpleNotificationService;
+using Amazon.SQS;
+using JustSaying.AwsTools;
+
+namespace JustSaying.TestingFramework
+{
+    /// <summary>
+    /// AwsCustomClient factory for running integration tests in continuous integration mode.
+    /// 
+    /// If environment variable "ci" is not defined, use default AWS profile name as specified by <code>IntegrationTestConfig.AwsProfileName</code>
+    /// 
+    /// Otherwise, construct AWS Profile using access and secret key by looking at "ci-access_key" and "ci-secret_key" environment variables.
+    /// </summary>
+    public class IntegrationAwsClientFactory : IAwsClientFactory
+    {
+        private readonly AWSCredentials _credentials;
+        private readonly string _ci = "ci";
+        private readonly string _ciAccesskey = "AWS_ACCESS_KEY_ID";
+        private readonly string _ciSecretkey = "AWS_SECRET_KEY";
+
+        public IntegrationAwsClientFactory()
+        {
+            var ci = System.Environment.GetEnvironmentVariable(_ci);
+            if (string.IsNullOrWhiteSpace(ci))
+                this._credentials = new StoredProfileAWSCredentials(IntegrationTestConfig.AwsProfileName);
+            else
+                _credentials = CredentialsFromEnvironment();
+        }
+
+        private AWSCredentials CredentialsFromEnvironment()
+        {
+            var accessKey = System.Environment.GetEnvironmentVariable(_ciAccesskey);
+            var secretKey = System.Environment.GetEnvironmentVariable(_ciSecretkey);
+            return new BasicAWSCredentials(accessKey, secretKey);
+        }
+
+        public IAmazonSimpleNotificationService GetSnsClient(RegionEndpoint region)
+        {
+            return AWSClientFactory.CreateAmazonSimpleNotificationServiceClient(_credentials, region);
+        }
+
+        public IAmazonSQS GetSqsClient(RegionEndpoint region)
+        {
+            return AWSClientFactory.CreateAmazonSQSClient(_credentials, region);
+        }
+    }
+}
