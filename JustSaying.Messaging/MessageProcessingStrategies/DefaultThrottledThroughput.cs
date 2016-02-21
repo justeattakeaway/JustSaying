b4851f04a4c19c1c77376bd32d964f37c24fb153
Anthony Steele <anthony.steele@just-eat.com>
2016-02-21 12:51:26 +0000

    Refactoring the last commit - etxracted methods, constants useful in more than 1 place

--- JustSaying.Messaging/MessageProcessingStrategies/DefaultThrottledThroughput.cs
+++ JustSaying.Messaging/MessageProcessingStrategies/DefaultThrottledThroughput.cs
@@ -5,13 +5,15 @@ namespace JustSaying.Messaging.MessageProcessingStrategies
 {
     public class DefaultThrottledThroughput : Throttled
     {
-        private const int MaxAmazonMessageCap = 10;
-        private static int ParallelHandlerExecutionPerCore = 8;
-
         public DefaultThrottledThroughput(IMessageMonitor messageMonitor) :
-            base(ParallelHandlerExecutionPerCore * Environment.ProcessorCount, MaxAmazonMessageCap, messageMonitor)
+            base(MaxActiveHandlersForProcessors(), MessageConstants.MaxAmazonMessageCap, messageMonitor)
         {
 
+        }
+
+        private static int MaxActiveHandlersForProcessors()
+        {
+            return Environment.ProcessorCount * MessageConstants.ParallelHandlerExecutionPerCore;
         }
     }
 }
