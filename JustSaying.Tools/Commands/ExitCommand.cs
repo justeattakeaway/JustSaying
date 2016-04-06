e48120eb641bffd65650a5190c626bcd136c4b07
Anthony Steele <anthony.steele@just-eat.com>
2016-04-06 21:03:57 +0100

    Remove parts of the project not used by the bug

--- JustSaying.Tools/Commands/ExitCommand.cs
+++
@@ -1,4 +0,0 @@
-namespace JustSaying.Tools.Commands
-{
-    public class ExitCommand : QuitCommand { }
-}
--- JustSaying/IPublishConfiguration.cs
+++
@@ -1,8 +0,0 @@
-namespace JustSaying
-{
-    public interface IPublishConfiguration
-    {
-        int PublishFailureReAttempts { get; set; }
-        int PublishFailureBackoffMilliseconds { get; set; }
-    }
-}
