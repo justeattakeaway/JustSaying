f74684f11c559780ce9e29f6d3a43131687cefea
Anthony Steele <anthony.steele@just-eat.com>
2016-04-06 21:29:17 +0100

    Ignore WhenAttemptingToInterrogateASubscriber test

--- JustSaying.AwsTools.UnitTests/MessageHandling/SqsNotificationListener/WhenAttemptingToInterrogateASubscriber.cs
+++ JustSaying.AwsTools.UnitTests/MessageHandling/SqsNotificationListener/WhenAttemptingToInterrogateASubscriber.cs
@@ -1,10 +1,12 @@
 using System.Linq;
 using JustBehave;
 using JustSaying.TestingFramework;
+using NUnit.Framework;
 using Shouldly;
 
 namespace JustSaying.AwsTools.UnitTests.MessageHandling.SqsNotificationListener
 {
+    [Ignore("Not buggy")]
     public class WhenAttemptingToInterrogateASubscriber : BaseQueuePollingTest
     {
         [Then]
