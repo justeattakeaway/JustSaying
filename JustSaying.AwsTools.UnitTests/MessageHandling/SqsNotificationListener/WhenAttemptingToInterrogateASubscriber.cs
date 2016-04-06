26b07241cea6f93aa8ff3ca7b0994505a1ffe68f
Anthony Steele <anthony.steele@just-eat.com>
2016-04-06 21:50:13 +0100

    This test is required to make it fail

--- JustSaying.AwsTools.UnitTests/MessageHandling/SqsNotificationListener/WhenAttemptingToInterrogateASubscriber.cs
+++ JustSaying.AwsTools.UnitTests/MessageHandling/SqsNotificationListener/WhenAttemptingToInterrogateASubscriber.cs
@@ -1,12 +1,10 @@
 using System.Linq;
 using JustBehave;
 using JustSaying.TestingFramework;
-using NUnit.Framework;
 using Shouldly;
 
 namespace JustSaying.AwsTools.UnitTests.MessageHandling.SqsNotificationListener
 {
-    [Ignore("Not buggy")]
     public class WhenAttemptingToInterrogateASubscriber : BaseQueuePollingTest
     {
         [Then]
