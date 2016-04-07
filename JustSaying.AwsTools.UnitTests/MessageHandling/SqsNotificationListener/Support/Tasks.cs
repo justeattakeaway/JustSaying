b7af1b201699528ee51f72460655ae34caa3f863
Anthony Steele <anthony.steele@just-eat.com>
2016-04-07 10:00:20 +0100

    Another trial - shorter timeout but don't throw

--- JustSaying.AwsTools.UnitTests/MessageHandling/SqsNotificationListener/Support/Tasks.cs
+++ JustSaying.AwsTools.UnitTests/MessageHandling/SqsNotificationListener/Support/Tasks.cs
@@ -6,24 +6,25 @@ namespace JustSaying.AwsTools.UnitTests.MessageHandling.SqsNotificationListener.
 {
     public static class Tasks
     {
-        private const int DefaultTimeoutMillis = 10000;
+        private const int DefaultTimeoutMillis = 1000;
         private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
-        public static async Task WaitWithTimeoutAsync(Task task)
+        public static async Task<bool> WaitWithTimeoutAsync(Task task)
         {
-            await WaitWithTimeoutAsync(task, TimeSpan.FromMilliseconds(DefaultTimeoutMillis));
+            return await WaitWithTimeoutAsync(task, TimeSpan.FromMilliseconds(DefaultTimeoutMillis));
         }
-        public static async Task WaitWithTimeoutAsync(Task task, TimeSpan timeoutDuration)
+        public static async Task<bool> WaitWithTimeoutAsync(Task task, TimeSpan timeoutDuration)
         {
             var timeoutTask = Task.Delay(timeoutDuration);
             var firstToComplete = await Task.WhenAny(task, timeoutTask).ConfigureAwait(false);
 
             if (firstToComplete == timeoutTask)
             {
-                var message = "Task did not complete before timeout of " + timeoutDuration;
-                Log.Error(message);
-                throw new TimeoutException(message);
+                Log.Error("Task did not complete before timeout of " + timeoutDuration);
+                return false;
             }
+
+            return true;
         }
 
         public static void DelaySendDone(TaskCompletionSource<object> doneSignal)
