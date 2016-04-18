d9c8a0f64b6fd448c4e13b5e4d55738bf7573d7a
Anthony Steele <anthony.steele@just-eat.com>
2016-04-18 16:06:31 +0100

    Added new async handler interface and adapter

---
+++ JustSaying.Messaging/MessageHandling/AsyncingHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace JustSaying.Messaging.MessageHandling
+{
+    public class AsyncingHandler<T> : IAsyncHandler<T>
+    {
+        private readonly IHandler<T> _syncHandler;
+
+        public AsyncingHandler(IHandler<T> syncHandler)
+        {
+            _syncHandler = syncHandler;
+        }
+
+        public Task<bool> Handle(T message)
+        {
+            return Task.FromResult(_syncHandler.Handle(message));
+        }
+    }
+}
