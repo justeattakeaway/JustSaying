2ad53282a937368290c9941e5dceb30f29fe3db9
Anthony Steele <anthony.steele@just-eat.com>
2016-07-30 16:27:38 +0100

    Rebased

--- JustSaying/JustSayingFluentlyExtensions.cs
+++ JustSaying/JustSayingFluentlyExtensions.cs
@@ -1,3 +1,6 @@
+using JustSaying.Messaging.MessageHandling;
+using JustSaying.Models;
+
 namespace JustSaying
 {
     public static class JustSayingFluentlyExtensions
@@ -5,6 +8,13 @@ namespace JustSaying
         public static IFluentSubscription IntoDefaultQueue(this ISubscriberIntoQueue subscriber)
         {
             return subscriber.IntoQueue(string.Empty);
+        }
+
+        public static IHaveFulfilledSubscriptionRequirements WithMessageHandlers<T>(
+             this IFluentSubscription sub, params IHandlerAsync<T>[] handlers) where T : Message
+        {
+            var listHandler = new ListHandler<T>(handlers);
+            return sub.WithMessageHandler(listHandler);
         }
     }
 }
