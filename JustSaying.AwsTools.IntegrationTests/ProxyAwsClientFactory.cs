bbf5c6cea862cc7e518c6663679a2081ceef8bf4
Anthony Steele <anthony.steele@just-eat.com>
2016-08-27 14:10:36 +0100

    Fixed some integration tests ProxyAwsClientFactory also needs to mock the async version of AWS SDK methods

--- JustSaying.AwsTools.IntegrationTests/ProxyAwsClientFactory.cs
+++ JustSaying.AwsTools.IntegrationTests/ProxyAwsClientFactory.cs
@@ -11,40 +11,35 @@ namespace JustSaying.AwsTools.IntegrationTests
     /// <summary>
     /// An AWS Client Factory which forwards all AWS calls to SNS/SQS clients
     /// and stores all calls in a dictionary.
-    /// 
+    ///
     /// Use to inspect what operations and which arguments have been passed
     /// </summary>
     public class ProxyAwsClientFactory : IAwsClientFactory
     {
         /// <summary>
         /// Root dictionary key is aws operation, such as ListTopics, GetQueue, etc.
-        /// 
-        /// Each inner dictionary contains a grouped collection of calls to that operator. Key for the grouping 
+        ///
+        /// Each inner dictionary contains a grouped collection of calls to that operator. Key for the grouping
         /// is chosen on a per operation basis, but in many cases this is the queue or topic name.
-        /// 
+        ///
         /// List of objects is the full list of arguments when that call was made.
         /// </summary>
-        private Dictionary<string, Dictionary<string, List<object>>> counters;
+        private readonly Dictionary<string, Dictionary<string, List<object>>> _counters
+            = new Dictionary<string, Dictionary<string, List<object>>>();
 
-        public ProxyAwsClientFactory()
-        {
-            counters = new Dictionary<string, Dictionary<string, List<object>>>();
-        }
-
-
-        public Dictionary<string, Dictionary<string, List<object>>> Counters => counters;
+        public Dictionary<string, Dictionary<string, List<object>>> Counters => _counters;
 
         public IAmazonSimpleNotificationService GetSnsClient(RegionEndpoint region)
         {
             var innerClient = CreateMeABus.DefaultClientFactory().GetSnsClient(region);
             var client = Substitute.For<IAmazonSimpleNotificationService>();
 
-            client.CreateTopic(Arg.Any<CreateTopicRequest>())
-                .ReturnsForAnyArgs(r => innerClient.CreateTopic(r.Arg<CreateTopicRequest>()))
+            client.CreateTopicAsync(Arg.Any<CreateTopicRequest>())
+                .ReturnsForAnyArgs(r => innerClient.CreateTopicAsync(r.Arg<CreateTopicRequest>()))
                 .AndDoes(r => Increment("CreateTopic", r.Arg<CreateTopicRequest>().Name, r.Arg<CreateTopicRequest>()));
 
-            client.FindTopic(Arg.Any<string>())
-                .ReturnsForAnyArgs(r => innerClient.FindTopic(r.Arg<string>()))
+            client.FindTopicAsync(Arg.Any<string>())
+                .ReturnsForAnyArgs(r => innerClient.FindTopicAsync(r.Arg<string>()))
                 .AndDoes(r => Increment("FindTopic", r.Arg<string>(), r.Arg<string>()));
 
             return client;
@@ -52,12 +47,16 @@ namespace JustSaying.AwsTools.IntegrationTests
 
         private void Increment(string operationName, string paramKey, params object[] extraParams)
         {
-            if (counters.ContainsKey(operationName) == false)
-                counters[operationName] = new Dictionary<string, List<object>>();
+            if (!_counters.ContainsKey(operationName))
+            {
+                _counters[operationName] = new Dictionary<string, List<object>>();
+            }
 
-            var operation = counters[operationName];
-            if (operation.ContainsKey(paramKey) == false)
+            var operation = _counters[operationName];
+            if (!operation.ContainsKey(paramKey))
+            {
                 operation.Add(paramKey, new List<object>());
+            }
 
             var paramOperation = operation[paramKey];
             paramOperation.Add(extraParams);
@@ -68,16 +67,16 @@ namespace JustSaying.AwsTools.IntegrationTests
             var innerClient = CreateMeABus.DefaultClientFactory().GetSqsClient(region);
             var client = Substitute.For<IAmazonSQS>();
 
-            client.ListQueues(Arg.Any<ListQueuesRequest>())
-                .ReturnsForAnyArgs(r => innerClient.ListQueues(r.Arg<ListQueuesRequest>()))
+            client.ListQueuesAsync(Arg.Any<ListQueuesRequest>())
+                .ReturnsForAnyArgs(r => innerClient.ListQueuesAsync(r.Arg<ListQueuesRequest>()))
                 .AndDoes(r => Increment("ListQueues", r.Arg<ListQueuesRequest>().QueueNamePrefix, r.Arg<ListQueuesRequest>()));
 
-            client.CreateQueue(Arg.Any<CreateQueueRequest>())
-                .ReturnsForAnyArgs(r => innerClient.CreateQueue(r.Arg<CreateQueueRequest>()))
+            client.CreateQueueAsync(Arg.Any<CreateQueueRequest>())
+                .ReturnsForAnyArgs(r => innerClient.CreateQueueAsync(r.Arg<CreateQueueRequest>()))
                 .AndDoes(r => Increment("CreateQueue", r.Arg<CreateQueueRequest>().QueueName, r.Arg<CreateQueueRequest>()));
 
-            client.GetQueueAttributes(Arg.Any<GetQueueAttributesRequest>())
-                .ReturnsForAnyArgs(r => innerClient.GetQueueAttributes(r.Arg<GetQueueAttributesRequest>()))
+            client.GetQueueAttributesAsync(Arg.Any<GetQueueAttributesRequest>())
+                .ReturnsForAnyArgs(r => innerClient.GetQueueAttributesAsync(r.Arg<GetQueueAttributesRequest>()))
                 .AndDoes(r => Increment("GetQueueAttributes", r.Arg<GetQueueAttributesRequest>().QueueUrl, r.Arg<GetQueueAttributesRequest>()));
 
             client.ReceiveMessageAsync(Arg.Any<ReceiveMessageRequest>())
