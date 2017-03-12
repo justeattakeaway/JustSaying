c2be35bbeb9826a8b4654324cedc8fe8304b20d4
Stuart Lang <stuart.lang@just-eat.com>
2017-03-12 17:20:55 +0000

    csproj love

--- JustSaying.Models/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("JustSaying.Models")]
-[assembly: AssemblyDescription("JustSaying is a light-weight service bus on top of AWS services that allows communication via messaging in a distributed architecture.")]
-[assembly: AssemblyCompany("JUST EAT plc")]
-[assembly: AssemblyCopyright("Copyright © JUST EAT plc 2013")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.0.0")]
