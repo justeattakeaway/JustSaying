9a9be6b413613223a5be312e32db0b0e4ac00996
London Coder <web.expert@me.com>
2017-03-24 11:34:16 +0000

    Improve performance (for cases when topic count is large) (#311)
    * Update topic existence check to just attempt a create, as should be faster than iterating through the pages using ListTopics request(s)
    
    * Fix (already broken) integration tests and switch queue existence check to just fetch url rather than call List()
    
    * Fix more broken integration tests (counting methods not particularly good way of testing but just going to fix existing assertions to get green build for now)
    
    * missed test file
    
    * Fix broken tests
    
    * extra null check
    
    * duplicated call!
    
    * formatting
    
    * Update topic creation call to allow for cases when client doesn't have permission to create, and will fallback to just a topic existence check
    
    * more formatting/stylistic changes
    
    * additional integration test for the fallback of existence check if topic creation call fails due to insufficient permissions
    
    * minor tidy-up of conditional check

---
+++ JustSaying.AwsTools.IntegrationTests/NoTopicCreationAwsClientFactory.cs
@@ -0,0 +1,37 @@
+﻿using Amazon;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using Amazon.SQS;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+
+namespace JustSaying.AwsTools.IntegrationTests
+{
+    public class NoTopicCreationAwsClientFactory : IAwsClientFactory
+    {
+
+        public IAmazonSimpleNotificationService GetSnsClient(RegionEndpoint region)
+        {
+            var innerClient = CreateMeABus.DefaultClientFactory().GetSnsClient(region);
+            var client = Substitute.For<IAmazonSimpleNotificationService>();
+
+            client.CreateTopic(Arg.Any<CreateTopicRequest>())
+                .ThrowsForAnyArgs(x => new AuthorizationErrorException("Denied"));
+
+            client.FindTopic(Arg.Any<string>())
+                .ReturnsForAnyArgs(r => innerClient.FindTopic(r.Arg<string>()));
+
+            client.GetTopicAttributes(Arg.Any<string>())
+                .ReturnsForAnyArgs(r => innerClient.GetTopicAttributes(r.Arg<string>()));
+
+            return client;
+        }
+
+
+        public IAmazonSQS GetSqsClient(RegionEndpoint region)
+        {
+            var innerClient = CreateMeABus.DefaultClientFactory().GetSqsClient(region);
+            return innerClient;
+        }
+    }
+}
