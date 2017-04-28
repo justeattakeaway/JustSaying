27600cd7606469703589c7ac719fad39c467b742
Anthony Steele <anthony.steele@just-eat.com>
2017-04-28 10:43:25 +0100

    No tests yet :(

--- build.sh
+++ build.sh
@@ -4,5 +4,3 @@ export configuration=Release
 
 dotnet restore JustSaying.sln --verbosity minimal || exit 1
 dotnet build JustSaying/JustSaying.csproj --output $artifacts --configuration $configuration --framework "netstandard1.6" || exit 1
-dotnet build JustSaying.UnitTests/JustSaying.UnitTests.csproj --output $artifacts --configuration $configuration --framework "netcoreapp1.0" || exit 1
-dotnet run --project JustSaying.UnitTests/JustSaying.UnitTests.csproj --configuration $configuration --framework "netcoreapp1.0"  || exit 1
