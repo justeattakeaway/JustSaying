fbce81bf16ed629db325c31b5c3747608b6f1c58
Joseph Woodward <joseph.woodward@just-eat.com>
2018-01-09 10:11:03 +0000

    Improve property annotation

--- JustSaying/AwsTools/QueueCreation/SnsWriteConfiguration.cs
+++ JustSaying/AwsTools/QueueCreation/SnsWriteConfiguration.cs
@@ -6,7 +6,7 @@ namespace JustSaying.AwsTools.QueueCreation
     public class SnsWriteConfiguration
     {
         /// <summary>
-        /// Extension point enabling custom error handling on a per notification basis, including ability handle the exception.
+        /// Extension point enabling custom error handling on a per notification basis, including ability handle raised exceptions.
         /// </summary>
         /// <returns>Boolean indicating whether the exception has been handled</returns>
         public Func<Exception, bool> HandleException { get; set; }
