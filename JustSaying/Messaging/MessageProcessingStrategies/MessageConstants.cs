cceb7415a7f547a6389a816b3b0a05b218b2af7c
Anthony Steele <Anthony@PlasticAvalon.org>
2018-11-05 09:46:35 +0000

    Props not fields

--- JustSaying/Messaging/MessageProcessingStrategies/MessageConstants.cs
+++ JustSaying/Messaging/MessageProcessingStrategies/MessageConstants.cs
@@ -2,7 +2,7 @@ namespace JustSaying.Messaging.MessageProcessingStrategies
 {
     public static class MessageConstants
     {
-        public const int MaxAmazonMessageCap = 10;
-        public const int ParallelHandlerExecutionPerCore = 8;
+        public static int MaxAmazonMessageCap => 10;
+        public static int ParallelHandlerExecutionPerCore => 8;
     }
 }
