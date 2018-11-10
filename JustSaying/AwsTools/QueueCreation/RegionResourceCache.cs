0e67fe283c881170798d7c73f3a3c37bee3faa4b
Anthony Steele <Anthony@PlasticAvalon.org>
2018-11-10 09:32:07 +0000

    OrdinalIgnoreCase

--- JustSaying/AwsTools/QueueCreation/RegionResourceCache.cs
+++ JustSaying/AwsTools/QueueCreation/RegionResourceCache.cs
@@ -6,7 +6,7 @@ namespace JustSaying.AwsTools.QueueCreation
     public sealed class RegionResourceCache<T>
     {
         private readonly ConcurrentDictionary<string, ConcurrentDictionary<string, T>> _regionsData
-            = new ConcurrentDictionary<string, ConcurrentDictionary<string, T>>(StringComparer.InvariantCultureIgnoreCase);
+            = new ConcurrentDictionary<string, ConcurrentDictionary<string, T>>(StringComparer.OrdinalIgnoreCase);
 
         public T TryGetFromCache(string region, string key)
         {
@@ -22,7 +22,7 @@ namespace JustSaying.AwsTools.QueueCreation
         public void AddToCache(string region, string key, T value)
         {
             var regionDict = _regionsData.GetOrAdd(region,
-                r => new ConcurrentDictionary<string, T>(StringComparer.InvariantCultureIgnoreCase));
+                r => new ConcurrentDictionary<string, T>(StringComparer.OrdinalIgnoreCase));
 
             regionDict[key] = value;
         }
