651b2ccb396cee7282e4f2323ca6cffc004b2335
martincostello <martin@martincostello.com>
2018-11-21 16:48:41 +0000

    Only run integration tests on Linux
    The macOS image doesn't have Docker installed.

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ if [ "$dotnet_version" != "$CLI_VERSION" ]; then
     curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version "$CLI_VERSION" --install-dir "$DOTNET_INSTALL_DIR"
 fi
 
-if [ "$CI" != "" ]; then
+if [ "$CI" != "" -a "$TRAVIS_OS_NAME" == "linux" ]; then
     docker pull pafortin/goaws
     docker run -d --name goaws -p 4100:4100 pafortin/goaws
     export AWS_SERVICE_URL="http://localhost:4100"
