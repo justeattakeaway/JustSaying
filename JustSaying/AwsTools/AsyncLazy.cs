9d475b5b9ba74e750bd8a5b9818a94292699f354
Stuart Lang <stuart.b.lang@gmail.com>
2018-11-24 18:45:17 +0000

    AsyncLazy simplification

--- JustSaying/AwsTools/AsyncLazy.cs
+++ JustSaying/AwsTools/AsyncLazy.cs
@@ -3,14 +3,9 @@ using System.Threading.Tasks;
 
 namespace JustSaying.AwsTools
 {
-    // TODO Does this still meet async best-practice?
-
     internal sealed class AsyncLazy<T> : Lazy<Task<T>>
     {
-        public AsyncLazy(Func<T> valueFactory) :
-            base(() => Task.Factory.StartNew(valueFactory)) { }
-
         public AsyncLazy(Func<Task<T>> taskFactory) :
-            base(() => Task.Factory.StartNew(() => taskFactory()).Unwrap()) { }
+            base(taskFactory) { }
     }
 }
