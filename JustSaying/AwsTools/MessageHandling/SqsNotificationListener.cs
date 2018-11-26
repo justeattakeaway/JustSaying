269c817f2d15961ecbc1c466db418b1646dc8638
martincostello <martin@martincostello.com>
2018-11-26 22:02:02 +0000

    Remove unused usings
    Remove unused using statements.

--- JustSaying/AwsTools/MessageHandling/SqsNotificationListener.cs
+++ JustSaying/AwsTools/MessageHandling/SqsNotificationListener.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Globalization;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Amazon.SQS;
