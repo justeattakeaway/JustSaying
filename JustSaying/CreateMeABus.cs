536a98924816175969a25adaa9a609c1ace84547
Martin Costello <martin@martincostello.com>
2018-11-29 08:37:35 +0000

    Remove commented out [Obsolete] attributes
    Remove the commented-out [Obsolete] attributes as we'll likely delete this class as part of v7 anyway.

--- JustSaying/CreateMeABus.cs
+++ JustSaying/CreateMeABus.cs
@@ -9,11 +9,9 @@ namespace JustSaying
         /// <summary>
         /// Allows to override default <see cref="IAwsClientFactory"/> globally.
         /// </summary>
-        ////[Obsolete("Use the BusBuilder class to create message buses.")]
         public static Func<IAwsClientFactory> DefaultClientFactory { get; set; }
             = () => new DefaultAwsClientFactory();
 
-        ////[Obsolete("Use the BusBuilder class to create message buses.")]
         public static JustSayingFluentlyDependencies WithLogging(ILoggerFactory loggerFactory) =>
             new JustSayingFluentlyDependencies { LoggerFactory = loggerFactory};
     }
