1b61c41c69aa58c89e1c638c21f07e245550032d
Martin Costello <martin@martincostello.com>
2018-12-13 10:14:32 +0000

    Use var instead of explicit type
    Use var instead of the explicit type for the handler resolver.

--- JustSaying/Fluent/SubscriptionsBuilder.cs
+++ JustSaying/Fluent/SubscriptionsBuilder.cs
@@ -118,7 +118,7 @@ namespace JustSaying.Fluent
         /// </exception>
         internal void Configure(JustSayingFluently bus)
         {
-            IHandlerResolver resolver = Parent.ServiceResolver.ResolveService<IHandlerResolver>();
+            var resolver = Parent.ServiceResolver.ResolveService<IHandlerResolver>();
 
             if (resolver == null)
             {
