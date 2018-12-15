8aca1d6bfc94dd13a51cbfc152e074f7180c75cf
martincostello <martin@martincostello.com>
2018-12-15 11:50:13 +0000

    Fix validation
    Remove validation from the builders as because they might compose, they might not become valid until later and thus block activation.

--- JustSaying/Fluent/SqsConfigurationBuilder.cs
+++ JustSaying/Fluent/SqsConfigurationBuilder.cs
@@ -129,8 +129,6 @@ namespace JustSaying.Fluent
             {
                 config.VisibilityTimeout = VisibilityTimeout.Value;
             }
-
-            config.Validate();
         }
     }
 }
