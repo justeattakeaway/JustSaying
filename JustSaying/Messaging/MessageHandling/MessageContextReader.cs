b448aa383eb26aad06d09d41833233b6209a3cec
Anthony Steele <anthony.steele@just-eat.com>
2018-12-20 11:33:33 +0000

    Wire up interface to impl

--- JustSaying/Messaging/MessageHandling/MessageContextReader.cs
+++ JustSaying/Messaging/MessageHandling/MessageContextReader.cs
@@ -2,9 +2,7 @@ using System.Threading;
 
 namespace JustSaying.Messaging.MessageHandling
 {
-#pragma warning disable CA1822 // Member Read does not access instance data and can be marked as static
-
-    public class MessageContextReader
+    public class MessageContextReader: IMessageContextReader
     {
         private static readonly AsyncLocal<MessageContext> Context = new AsyncLocal<MessageContext>();
 
