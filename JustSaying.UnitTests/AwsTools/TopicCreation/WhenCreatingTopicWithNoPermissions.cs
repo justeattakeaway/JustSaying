d84fb72c689529230e1499ec10d1b546c631f160
martincostello <martin@martincostello.com>
2019-02-24 16:48:25 +0000

    Recreate integration test as unit test
    Change integration test to a unit test because it was using mocks to simulate AWS behaviour, so was more of a unit test.

---
+++ JustSaying.UnitTests/AwsTools/TopicCreation/WhenCreatingTopicWithNoPermissions.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Threading.Tasks;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using JustSaying.AwsTools.MessageHandling;
+using JustSaying.Messaging.MessageSerialization;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace JustSaying.UnitTests.AwsTools.TopicCreation
+{
+    public class WhenCreatingTopicWithNoPermissions
+    {
+        public WhenCreatingTopicWithNoPermissions(ITestOutputHelper outputHelper)
+        {
+            OutputHelper = outputHelper;
+        }
+
+        private ITestOutputHelper OutputHelper { get; }
+
+        [Fact]
+        public async Task Cannot_Create_Topic_Because_It_Exists()
+        {
+            // Arrange
+            string topicName = Guid.NewGuid().ToString();
+            ILoggerFactory loggerFactory = OutputHelper.ToLoggerFactory();
+
+            var subjectProvider = new NonGenericMessageSubjectProvider();
+            var serializationRegister = new MessageSerializationRegister(subjectProvider);
+
+            IAmazonSimpleNotificationService client = CreateSnsClient(exists: true);
+
+            var topic = new SnsTopicByName(
+                topicName,
+                client,
+                serializationRegister,
+                loggerFactory,
+                subjectProvider);
+
+            // Act
+            bool actual = await topic.CreateAsync();
+
+            // Assert
+            actual.ShouldBeFalse();
+            topic.Arn.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async Task Cannot_Create_Topic_Because_Not_Authorized()
+        {
+            // Arrange
+            string topicName = Guid.NewGuid().ToString();
+            ILoggerFactory loggerFactory = OutputHelper.ToLoggerFactory();
+
+            var subjectProvider = new NonGenericMessageSubjectProvider();
+            var serializationRegister = new MessageSerializationRegister(subjectProvider);
+
+            IAmazonSimpleNotificationService client = CreateSnsClient(exists: false);
+
+            var topic = new SnsTopicByName(
+                topicName,
+                client,
+                serializationRegister,
+                loggerFactory,
+                subjectProvider);
+
+            // Act and Assert
+            await Assert.ThrowsAsync<InvalidOperationException>(() => topic.CreateAsync());
+        }
+
+        private IAmazonSimpleNotificationService CreateSnsClient(bool exists)
+        {
+            var client = Substitute.For<IAmazonSimpleNotificationService>();
+
+            client.CreateTopicAsync(Arg.Any<CreateTopicRequest>())
+                  .ThrowsForAnyArgs((_) => new AuthorizationErrorException("Denied"));
+
+            if (exists)
+            {
+                client.FindTopicAsync(Arg.Any<string>())
+                      .ReturnsForAnyArgs(Task.FromResult(new Topic() { TopicArn = "foo" }));
+            }
+            else
+            {
+                client.FindTopicAsync(Arg.Any<string>())
+                      .ReturnsForAnyArgs(Task.FromResult<Topic>(null));
+            }
+
+            return client;
+        }
+    }
+}
