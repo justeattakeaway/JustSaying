6023cfafb6c7e0f130d50a4dcbb0dc6d770ae1aa
martincostello <martin@martincostello.com>
2019-06-22 13:59:46 +0100

    Switch to coverlet
    Switch to coverlet for code coverage from OpenCover.

--- Build.ps1
+++ Build.ps1
@@ -114,33 +114,19 @@ function DotNetTest {
         $nugetPath = Join-Path $env:USERPROFILE ".nuget\packages"
         $propsFile = Join-Path $solutionPath "Directory.Build.props"
 
-        $openCoverVersion = (Select-Xml -Path $propsFile -XPath "//PackageReference[@Include='OpenCover']/@Version").Node.'#text'
-        $openCoverPath = Join-Path $nugetPath "OpenCover\$openCoverVersion\tools\OpenCover.Console.exe"
-
         $reportGeneratorVersion = (Select-Xml -Path $propsFile -XPath "//PackageReference[@Include='ReportGenerator']/@Version").Node.'#text'
         $reportGeneratorPath = Join-Path $nugetPath "ReportGenerator\$reportGeneratorVersion\tools\netcoreapp2.0\ReportGenerator.dll"
 
-        $coverageOutput = Join-Path $OutputPath "code-coverage.xml"
+        $coverageOutput = Join-Path $OutputPath "coverage.cobertura.xml"
         $reportOutput = Join-Path $OutputPath "coverage"
 
-        & $openCoverPath `
-            `"-target:$dotnetPath`" `
-            `"-targetargs:test $Project --output $OutputPath`" `
-            -output:$coverageOutput `
-            `"-excludebyattribute:System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage*`" `
-            -hideskipped:All `
-            -mergebyhash `
-            -mergeoutput `
-            -oldstyle `
-            -register:user `
-            -skipautoprops `
-            `"-filter:+[JustSaying]* -[*Test*]*`"
+        & $dotnetPath test $Project --output $OutputPath
 
         & $dotnet `
             $reportGeneratorPath `
             `"-reports:$coverageOutput`" `
             `"-targetdir:$reportOutput`" `
-            -reporttypes:HTML`;Cobertura `
+            -reporttypes:HTML `
             -verbosity:Warning
     }
 
@@ -163,6 +149,7 @@ if (($null -ne $env:CI) -And ($EnableIntegrationTests -eq $true)) {
 
 if ($SkipTests -eq $false) {
     Write-Host "Running tests..." -ForegroundColor Green
+    Remove-Item -Path (Join-Path $OutputPath "coverage.json") -Force -ErrorAction SilentlyContinue | Out-Null
     ForEach ($testProject in $testProjects) {
         DotNetTest $testProject
     }
