8caffffdac2073709d223541a63bfa63175efff6
martincostello <martin@martincostello.com>
2019-08-25 12:52:14 +0100

    Add integration tests for System.Text.Json
    Add integration tests for topics and queues when using System.Text.Json for JSON serialization.

---
+++ JustSaying.IntegrationTests/Fluent/Publishing/WhenAMessageIsPublishedToATopicWithSystemTextJson.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Threading.Tasks;
+using JustSaying.Fluent;
+using JustSaying.Messaging;
+using JustSaying.TestingFramework;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using Xunit.Abstractions;
+
+namespace JustSaying.IntegrationTests.Fluent.Publishing
+{
+    public class WhenAMessageIsPublishedToATopicWithSystemTextJson : IntegrationTestBase
+    {
+        public WhenAMessageIsPublishedToATopicWithSystemTextJson(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        [AwsFact]
+        public async Task Then_The_Message_Is_Handled()
+        {
+            // Arrange
+            var completionSource = new TaskCompletionSource<object>();
+            var handler = CreateHandler<SimpleMessage>(completionSource);
+
+            var services = GivenJustSaying()
+                .ConfigureJustSaying(
+                    (builder) =>
+                    {
+                        builder.WithLoopbackTopic<SimpleMessage>(UniqueName)
+                               .Services((configure) => configure.WithSystemTextJson());
+                    })
+                .AddSingleton(handler);
+
+            string content = Guid.NewGuid().ToString();
+
+            var message = new SimpleMessage()
+            {
+                Content = content
+            };
+
+            await WhenAsync(
+                services,
+                async (publisher, listener, cancellationToken) =>
+                {
+                    listener.Start(cancellationToken);
+
+                    // Act
+                    await publisher.PublishAsync(message, cancellationToken);
+
+                    // Assert
+                    completionSource.Task.Wait(cancellationToken);
+
+                    await handler.Received().Handle(Arg.Is<SimpleMessage>((m) => m.Content == content));
+                });
+        }
+    }
+}
