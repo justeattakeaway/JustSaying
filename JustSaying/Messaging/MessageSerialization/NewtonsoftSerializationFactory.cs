f263115a5e7af300b8aed4fbaccdc2b936178b38
martincostello <martin@martincostello.com>
2019-08-25 12:50:18 +0100

    Improve DI for JSON serialization
    Extend ServiceBuilder and the message serialiation factories to make it easier to configure a custom serializer.

--- JustSaying/Messaging/MessageSerialization/NewtonsoftSerializationFactory.cs
+++ JustSaying/Messaging/MessageSerialization/NewtonsoftSerializationFactory.cs
@@ -1,10 +1,21 @@
 using JustSaying.Models;
+using Newtonsoft.Json;
 
 namespace JustSaying.Messaging.MessageSerialization
 {
     public class NewtonsoftSerializationFactory : IMessageSerializationFactory
     {
-        private readonly NewtonsoftSerializer _serializer = new NewtonsoftSerializer();
+        private readonly NewtonsoftSerializer _serializer;
+
+        public NewtonsoftSerializationFactory()
+            : this(null)
+        {
+        }
+
+        public NewtonsoftSerializationFactory(JsonSerializerSettings settings)
+        {
+            _serializer = new NewtonsoftSerializer(settings);
+        }
 
         public IMessageSerializer GetSerializer<T>() where T : Message => _serializer;
     }
--- JustSaying/Messaging/MessageSerialization/SystemTextJsonSerializationFactory.cs
+++ JustSaying/Messaging/MessageSerialization/SystemTextJsonSerializationFactory.cs
@@ -1,10 +1,21 @@
+using System.Text.Json;
 using JustSaying.Models;
 
 namespace JustSaying.Messaging.MessageSerialization
 {
     public class SystemTextJsonSerializationFactory : IMessageSerializationFactory
     {
-        private readonly SystemTextJsonSerializer _serializer = new SystemTextJsonSerializer();
+        private readonly SystemTextJsonSerializer _serializer;
+
+        public SystemTextJsonSerializationFactory()
+            : this(null)
+        {
+        }
+
+        public SystemTextJsonSerializationFactory(JsonSerializerOptions options)
+        {
+            _serializer = new SystemTextJsonSerializer(options);
+        }
 
         public IMessageSerializer GetSerializer<T>() where T : Message => _serializer;
     }
