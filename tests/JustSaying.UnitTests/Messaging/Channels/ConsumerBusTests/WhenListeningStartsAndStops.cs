b6a43720439cd5452a43848ac823baed070f411f
maisie-sadler <maisie.sadler@just-eat.com>
2020-02-27 16:24:26 +0000

    WhenListeningStartsAndStops

---
+++ tests/JustSaying.UnitTests/Messaging/Channels/ConsumerBusTests/WhenListeningStartsAndStops.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.SQS.Model;
+using JustSaying.AwsTools.MessageHandling;
+using JustSaying.Messaging.MessageProcessingStrategies;
+using NSubstitute;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace JustSaying.UnitTests.Messaging.Channels.ConsumerBusTests
+{
+    public class WhenListeningStartsAndStops : BaseConsumerBusTests
+    {
+        private ISqsQueue _queue;
+
+        public WhenListeningStartsAndStops(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
+        {
+        }
+        private const string _messageContentsRunning = @"Message Contents Running";
+        private const string _messageContentsAfterStop = @"Message Contents After Stop";
+
+        private int _expectedMaxMessageCount;
+        private bool _running = false;
+
+        protected override void Given()
+        {
+            // we expect to get max 10 messages per batch
+            // except on single-core machines when we top out at ParallelHandlerExecutionPerCore=8
+            _expectedMaxMessageCount = Math.Min(MessageConstants.MaxAmazonMessageCap,
+                Environment.ProcessorCount * MessageConstants.ParallelHandlerExecutionPerCore);
+
+            var response1 = new List<Message> { new Message { Body = _messageContentsRunning } };
+            var response2 = new List<Message> { new Message { Body = _messageContentsAfterStop } };
+
+            _queue = Substitute.For<ISqsQueue>();
+            _queue.GetMessages(Arg.Any<int>(), Arg.Any<List<string>>(), Arg.Any<CancellationToken>())
+                .Returns(_ =>
+                {
+                    return _running ? response1 : response2;
+                });
+            _queue.Uri.Returns(new Uri("http://foo.com"));
+
+            Queues.Add(_queue);
+        }
+
+        protected override async Task WhenAsync()
+        {
+            _running = true;
+            var cts = new CancellationTokenSource();
+
+            SystemUnderTest.Start(cts.Token);
+
+            // todo: should this be needed/should Start only complete when everything is running?
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+
+            _running = false;
+            cts.Cancel();
+
+            await SystemUnderTest.Completion;
+        }
+
+        [Fact]
+        public void MessagesAreReceived()
+        {
+            _queue.Received()
+                .GetMessages(Arg.Any<int>(), Arg.Any<List<string>>(), Arg.Any<CancellationToken>());
+        }
+
+        [Fact]
+        public void TheMaxMessageAllowanceIsGrabbed()
+        {
+            _queue.Received()
+                .GetMessages(Arg.Is<int>(count => count == _expectedMaxMessageCount), Arg.Any<List<string>>(), Arg.Any<CancellationToken>());
+        }
+
+        [Fact]
+        public void MessageIsProcessed()
+        {
+            SerializationRegister.Received()
+                .DeserializeMessage(_messageContentsRunning);
+            SerializationRegister.Received()
+                .DeserializeMessage(_messageContentsAfterStop);
+        }
+    }
+}
