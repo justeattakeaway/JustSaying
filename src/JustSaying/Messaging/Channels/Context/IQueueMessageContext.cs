adf0c1e38dc18587d64b56ebbdfde6d16a3ad7e7
George Kinsman <george@georgekinsman.com>
2020-05-12 11:33:11 +0100

    - Add docs for ISqsQueue
    - Add docs for IQueueMessageContext

--- src/JustSaying/Messaging/Channels/Context/IQueueMessageContext.cs
+++ src/JustSaying/Messaging/Channels/Context/IQueueMessageContext.cs
@@ -11,16 +11,37 @@ namespace JustSaying.Messaging.Channels.Context
     /// </summary>
     public interface IQueueMessageContext
     {
+        /// <summary>
+        /// The raw <see cref="Amazon.SQS.Model"/> that is sent over the wire.
+        /// </summary>
         Message Message { get; }
 
+        /// <summary>
+        /// A collection of <see cref="MessageAttributeValue"/>'s that were sent with this message
+        /// </summary>
         MessageAttributes MessageAttributes { get; }
 
+        /// <summary>
+        /// Updates this messages visibility so that it won't be received by other subscribers before the new timeout expires
+        /// </summary>
+        /// <param name="visibilityTimeout">How far into the future to prevent others from receiving this message.</param>
+        /// <returns>A <see cref="Task"/> that completes when the update is completed</returns>
         Task ChangeMessageVisibilityAsync(TimeSpan visibilityTimeout);
 
+        /// <summary>
+        /// Deletes this message from SQS
+        /// </summary>
+        /// <returns>A <see cref="Task"/> that completes when the message is deleted</returns>
         Task DeleteMessageFromQueueAsync();
 
+        /// <summary>
+        /// The full URI of the SQS queue that this message was received from.
+        /// </summary>
         Uri QueueUri { get; }
 
+        /// <summary>
+        /// The name of the queue that this message was received on
+        /// </summary>
         string QueueName { get; }
     }
 }
