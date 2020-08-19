0d48a0c3a278d1d52e9572a93bbc1c2b15c16103
George Kinsman <george@georgekinsman.com>
2020-08-19 15:17:15 +0100

    - Add docs for QueueWithAsyncStartup

--- src/JustSaying/AwsTools/MessageHandling/QueueWithAsyncStartup.cs
+++ src/JustSaying/AwsTools/MessageHandling/QueueWithAsyncStartup.cs
@@ -2,21 +2,44 @@ using System.Threading.Tasks;
 
 namespace JustSaying.AwsTools.MessageHandling
 {
+    /// <summary>
+    /// Represents an <see cref="ISqsQueue"/> with an associated startup task that must be run before
+    /// the queue is ready to be used.
+    /// </summary>
+    /// <typeparam name="TQueue"></typeparam>
     public class QueueWithAsyncStartup<TQueue> where TQueue : ISqsQueue
     {
+        /// <summary>
+        /// Creates an instance of <see cref="QueueWithAsyncStartup{TQueue}"/> for which the <see cref="TQueue"/>
+        /// is already ready, and doesn't need to be initialised.
+        /// </summary>
+        /// <param name="queue"></param>
         public QueueWithAsyncStartup(TQueue queue)
         {
             StartupTask = Task.CompletedTask;
             Queue = queue;
         }
 
+        /// <summary>
+        /// Creates an instance of <see cref="QueueWithAsyncStartup{TQueue}"/> that requires the <see cref="TQueue"/>
+        /// to be initialised by awaiting the <see cref="StartupTask"/>.
+        /// </summary>
+        /// <param name="startupTask">The <see cref="Task"/> that must be awaited on startup.</param>
+        /// <param name="queue">An <see cref="TQueue"/> that must be initialised on startup.</param>
         public QueueWithAsyncStartup(Task startupTask, TQueue queue)
         {
             StartupTask = startupTask;
             Queue = queue;
         }
 
+        /// <summary>
+        /// A <see cref="Task"/> that must be run before the queue is ready.
+        /// </summary>
         public Task StartupTask { get; }
+        /// <summary>
+        /// An <see cref="ISqsQueue"/> that will be ready when the <see cref="StartupTask"/>
+        /// has been awaited.
+        /// </summary>
         public TQueue Queue { get; }
     }
 }
