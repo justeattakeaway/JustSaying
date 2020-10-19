4671d3ea8756b4e6d63f1b62c2412e0e0665a343
Chris Small <chris.small@just-eat.com>
2020-10-19 17:14:59 +0100

    docs: update IMessageMonitor docs per George's feedback

--- src/JustSaying/Messaging/Monitoring/IMessageMonitor.cs
+++ src/JustSaying/Messaging/Monitoring/IMessageMonitor.cs
@@ -1,5 +1,6 @@
 using System;
 using Amazon.SQS.Model;
+using JustSaying.Messaging.MessageHandling;
 
 namespace JustSaying.Messaging.Monitoring
 {
@@ -10,58 +11,62 @@ namespace JustSaying.Messaging.Monitoring
     public interface IMessageMonitor
     {
         /// <summary>
-        /// Monitors when a specific message type threw an exception while being handled
-        /// by a subscriber.
+        /// Called when a specific message type threw an exception while being handled
+        /// by a <see cref="IHandlerAsync{T}"/>.
         /// </summary>
         /// <param name="messageType">The type of the message that was being handled.</param>
         void HandleException(Type messageType);
 
         /// <summary>
-        /// Monitors when a particular message that threw an exception while being handled
-        /// by a subscriber.
+        /// Called when an error occurs while attempting to de-serialize a message for a subscriber.
         /// </summary>
         /// <param name="ex">The exception that was thrown.</param>
-        /// <param name="message">The message being handled when the exception was thrown.</param>
+        /// <param name="message">The <see cref="Amazon.SQS.Model.Message" /> being handled when the exception was thrown.</param>
         void HandleError(Exception ex, Message message);
 
         /// <summary>
-        /// Monitors the length of time a message took to be handled by a subscriber.
+        /// Called when a subscription handler has completed, with the time taken by the handler.
         /// </summary>
         /// <param name="duration">The length of time taken.</param>
         void HandleTime(TimeSpan duration);
 
         /// <summary>
-        /// Monitors when a message was not successfully published after waiting for
+        /// Called when a message was not successfully published after waiting for
         /// the <see cref="IPublishConfiguration.PublishFailureReAttempts">configured amount of retries</see>.
         /// </summary>
         void IssuePublishingMessage();
 
         /// <summary>
-        /// Monitors when each message is handled by a subscriber.
+        /// Called when each message is handled by a subscriber.
         /// </summary>
         /// <param name="message">The message that was handled.</param>
         void Handled(JustSaying.Models.Message message);
 
         /// <summary>
-        /// Monitors each time a message took over 1ms to be written to a ChannelWriter.
+        /// Called each time a message pipeline is full, and a subscriber must wait until
+        /// there is room to continue downloading the message. This method is only called if the
+        /// duration was longer than the internal timeout (currently 1ms).
         /// </summary>
         void IncrementThrottlingStatistic();
 
         /// <summary>
-        /// Monitors the length of time a message took to be written to a ChannelWriter,
-        /// if that duration was longer than 1ms.
+        /// Called each time a message pipeline is full, indicating the length of time the
+        /// message took to be written to the pipeline. This method is only called if the
+        /// duration was longer than the internal timeout (currently 1ms).
         /// </summary>
-        /// <param name="duration">The length of time the write took.</param>
+        /// <param name="duration">The length of time the message took to be written.</param>
         void HandleThrottlingTime(TimeSpan duration);
 
         /// <summary>
-        /// Monitors the length of time a message took to be published.
+        /// Called when a message is published, indicating the length of time the message
+        /// took to be published.
         /// </summary>
         /// <param name="duration">The length of time.</param>
         void PublishMessageTime(TimeSpan duration);
 
         /// <summary>
-        /// Monitors the length of time a message's content took to be retrieved from SQS.
+        /// Called when a message's content is downloaded, indicating the length of time it
+        /// took for the content to be retrieved from SQS.
         /// </summary>
         /// <param name="duration">The length of time</param>
         /// <param name="queueName">The name of the queue the message was retrieved from.</param>
