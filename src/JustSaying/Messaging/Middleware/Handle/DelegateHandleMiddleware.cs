cbddbabf88543b2b345c1c6596524742b87f23d3
George Kinsman <george@georgekinsman.com>
2020-10-22 12:25:22 +0100

    Update src/JustSaying/Messaging/Middleware/Handle/DelegateHandleMiddleware.cs
    Co-authored-by: Martin Costello <martin@martincostello.com>

--- src/JustSaying/Messaging/Middleware/Handle/DelegateHandleMiddleware.cs
+++ src/JustSaying/Messaging/Middleware/Handle/DelegateHandleMiddleware.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 namespace JustSaying.Messaging.Middleware.Handle
 {
     public class DelegateHandleMiddleware : MiddlewareBase<HandleMessageContext, bool>
-
     {
         protected override async Task<bool> RunInnerAsync(HandleMessageContext context, Func<CancellationToken, Task<bool>> func, CancellationToken stoppingToken)
         {
