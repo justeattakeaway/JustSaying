0eed837d0bd4292c913336b800bea60b2e43f46b
George Kinsman <george@georgekinsman.com>
2020-12-07 16:26:33 +0000

    - Add more xml docs

--- src/JustSaying/Messaging/Middleware/Handle/HandleMessageContext.cs
+++ src/JustSaying/Messaging/Middleware/Handle/HandleMessageContext.cs
@@ -5,6 +5,12 @@ namespace JustSaying.Messaging.Middleware
 {
     public sealed class HandleMessageContext
     {
+        /// <summary>
+        /// This class encapsulates a messages context as it passes through a middleware pipeline.
+        /// </summary>
+        /// <param name="message">The JustSaying message that was deserialized from SQS.</param>
+        /// <param name="messageType">The type of the JustSaying message contained in <see cref="Message"/>.</param>
+        /// <param name="queueName">The queue from which this message was received.</param>
         public HandleMessageContext(Message message, Type messageType, string queueName)
         {
             Message = message;
@@ -12,10 +18,19 @@ namespace JustSaying.Messaging.Middleware
             QueueName = queueName;
         }
 
+        /// <summary>
+        /// The queue name from which this message was received.
+        /// </summary>
         public string QueueName { get; }
 
+        /// <summary>
+        /// The type of the JustSaying message contained in <see cref="Message"/>.
+        /// </summary>
         public Type MessageType { get; }
 
+        /// <summary>
+        /// The JustSaying message that was deserialized from SQS.
+        /// </summary>
         public Message Message { get; }
     }
 }
--- src/JustSaying/Messaging/Middleware/Handle/HandleMessageContextExtensions.cs
+++ src/JustSaying/Messaging/Middleware/Handle/HandleMessageContextExtensions.cs
@@ -5,6 +5,13 @@ namespace JustSaying.Messaging.Middleware
 {
     public static class HandleMessageContextExtensions
     {
+        /// <summary>
+        /// A convenience method to get a message as <see cref="TMessage"/> from the context.
+        /// </summary>
+        /// <param name="context">The context to get the message from.</param>
+        /// <typeparam name="TMessage">The type of the message to try and get from the context.</typeparam>
+        /// <returns>An instance of <see cref="TMessage"/> or null if the message was not of type <see cref="TMessage"/></returns>
+        /// <exception cref="ArgumentNullException">The <see cref="context"/> object is null.</exception>
         public static TMessage MessageAs<TMessage>(this HandleMessageContext context) where TMessage : Message
         {
             if (context == null) throw new ArgumentNullException(nameof(context));
