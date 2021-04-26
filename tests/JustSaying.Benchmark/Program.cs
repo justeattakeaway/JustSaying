25838db16bf79ed72691a56e8bd2da71a2980140
Simon Cropp <simon.cropp@gmail.com>
2021-04-26 19:47:10 +1000

    top level methods

--- tests/JustSaying.Benchmark/Program.cs
+++ tests/JustSaying.Benchmark/Program.cs
@@ -1,30 +1,22 @@
 using System.Threading.Tasks;
 using CommandLine;
+using JustSaying.Benchmark;
 using Serilog;
 
-namespace JustSaying.Benchmark
-{
-    public class Program
-    {
-        static async Task Main(string[] args)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.Console()
-                .MinimumLevel.Warning()
-                .WriteTo.Seq("http://localhost:5341")
-                .Enrich.WithProperty("AppName", "JustSaying.Benchmark")
-                .CreateLogger();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .MinimumLevel.Warning()
+    .WriteTo.Seq("http://localhost:5341")
+    .Enrich.WithProperty("AppName", "JustSaying.Benchmark")
+    .CreateLogger();
 
-            try
-            {
-                await Parser.Default.ParseArguments<JustSayingBenchmark>(args)
-                    .MapResult(async a => await JustSayingBenchmark.RunTest(a),
-                        errs => Task.CompletedTask);
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-        }
-    }
+try
+{
+    await Parser.Default.ParseArguments<JustSayingBenchmark>(args)
+        .MapResult(async a => await JustSayingBenchmark.RunTest(a),
+            errs => Task.CompletedTask);
+}
+finally
+{
+    Log.CloseAndFlush();
 }
