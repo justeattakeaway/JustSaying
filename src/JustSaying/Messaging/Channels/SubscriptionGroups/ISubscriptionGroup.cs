f918f77e4f59f8126cbcc52f7359f2f3fbbcae17
Simon Cropp <simon.cropp@gmail.com>
2021-04-26 19:07:30 +1000

    fix broken xml docs

--- src/JustSaying/Messaging/Channels/SubscriptionGroups/ISubscriptionGroup.cs
+++ src/JustSaying/Messaging/Channels/SubscriptionGroups/ISubscriptionGroup.cs
@@ -1,6 +1,8 @@
 using System.Threading;
 using System.Threading.Tasks;
 using JustSaying.Messaging.Interrogation;
+using JustSaying.Messaging.Channels.Receive;
+using JustSaying.Messaging.Channels.Dispatch;
 
 namespace JustSaying.Messaging.Channels.SubscriptionGroups
 {
