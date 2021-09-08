2ed86064be1911dc7520b5d2ff336cf5df2dbd4b
George Kinsman <george.kinsman@justeattakeaway.com>
2021-09-08 23:25:00 +0100

    - Add docs for new ISqsQueue members

--- src/JustSaying/AwsTools/MessageHandling/ISqsQueue.cs
+++ src/JustSaying/AwsTools/MessageHandling/ISqsQueue.cs
@@ -33,8 +33,33 @@ namespace JustSaying.AwsTools.MessageHandling
         /// </summary>
         string Arn { get; }
 
+        /// <summary>
+        /// Deletes a message from a queue
+        /// </summary>
+        /// <param name="queueUrl">The URL of the queue to delete a message from</param>
+        /// <param name="receiptHandle">The receipt handle of the message to delete</param>
+        /// <param name="cancellationToken">A cancellation token to cancel this operation.</param>
+        /// <returns>A <see cref="Task"/> will complete when the message has been deleted, or the task has faulted.</returns>
         public Task DeleteMessageAsync(string queueUrl, string receiptHandle, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Tags a queue with one or more key=value pairs.
+        /// </summary>
+        /// <param name="queueUrl">The URL of the queue to delete a message from</param>
+        /// <param name="tags">A <see cref="Dictionary{string, string}"/> of tags to tag this queue with.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel this operation.</param>
+        /// <returns>A <see cref="Task"/> will complete when the queue has been tagged, or the task has faulted.</returns>
         public Task TagQueueAsync(string queueUrl, Dictionary<string, string> tags, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Downloads messages from a queue.
+        /// </summary>
+        /// <param name="queueUrl">The URL of the queue to delete a message from</param>
+        /// <param name="maxNumOfMessages">The maximum number of messages to try to download. Values larger than 10 will default to 10. </param>
+        /// <param name="secondsWaitTime">The number of seconds to wait for messages to be available before returning.</param>
+        /// <param name="attributesToLoad">A list of attributes to retrieve for the downloaded messages.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel this operation.</param>
+        /// <returns>A <see cref="Task"/> will complete when messages have been received, or the task has faulted.</returns>
         public Task<IList<Message>> ReceiveMessagesAsync(
             string queueUrl,
             int maxNumOfMessages,
@@ -42,6 +67,14 @@ namespace JustSaying.AwsTools.MessageHandling
             IList<string> attributesToLoad,
             CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Updates the visibility timeout of a message to prevent it from being re-handled until a future time.
+        /// </summary>
+        /// <param name="queueUrl">The URL of the queue to delete a message from</param>
+        /// <param name="receiptHandle">The receipt handle of the message that will be updated</param>
+        /// <param name="visibilityTimeoutInSeconds">The number of seconds until this message will be visible to other consumers</param>
+        /// <param name="cancellationToken">A cancellation token to cancel this operation.</param>
+        /// <returns></returns>
         public Task ChangeMessageVisibilityAsync(
             string queueUrl,
             string receiptHandle,
