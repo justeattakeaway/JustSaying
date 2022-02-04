6472e71929dc4f6a479295e999b04ffd63da2667
George Kinsman <george.kinsman@justeattakeaway.com>
2022-02-04 15:32:09 +0000

    - Privatise TypeExtensions

--- src/JustSaying/Extensions/TypeExtensions.cs
+++ src/JustSaying/Extensions/TypeExtensions.cs
@@ -1,6 +1,6 @@
 namespace JustSaying.Extensions;
 
-public static class TypeExtensions
+static class TypeExtensions
 {
     public static string ToSimpleName(this Type type) =>
         type.ToString().Replace($"{type.Namespace}.", "");
