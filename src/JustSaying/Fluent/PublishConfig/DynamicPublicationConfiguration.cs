e3d95b9be32bc68efac513fecc41a61b4de9cc1b
George Kinsman <george@georgekinsman.com>
2022-05-19 12:54:23 +0100

    Update src/JustSaying/Fluent/PublishConfig/DynamicPublicationConfiguration.cs
    Co-authored-by: Martin Costello <martin@martincostello.com>

--- src/JustSaying/Fluent/PublishConfig/DynamicPublicationConfiguration.cs
+++ src/JustSaying/Fluent/PublishConfig/DynamicPublicationConfiguration.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 
 namespace JustSaying.Fluent;
 
-internal class DynamicPublicationConfiguration : ITopicPublisher
+internal sealed class DynamicPublicationConfiguration : ITopicPublisher
 {
     public DynamicPublicationConfiguration(IMessagePublisher publisher)
     {
