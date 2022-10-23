8b7305d3af1e19b1672c12533400affca3b95c32
peterkneale <peterkneale@gmail.com>
2022-10-24 03:18:37 +1100

    Make IAM policy generation more testable

---
+++ src/JustSaying/AwsTools/MessageHandling/SnsPolicyBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace JustSaying.AwsTools.MessageHandling;
+
+internal static class SnsPolicyBuilder
+{
+    internal static string BuildPolicyJson(SnsPolicyDetails policyDetails)
+    {
+        var sourceAccountId = ExtractSourceAccountId(policyDetails.SourceArn);
+        var policyJson = $@"{{
+    ""Version"" : ""2012-10-17"",
+    ""Statement"" : [
+        {{
+            ""Sid"" : ""{Guid.NewGuid().ToString().Replace("-", "")}"",
+            ""Effect"" : ""Allow"",
+            ""Principal"" : {{
+                ""AWS"" : ""*""
+            }},
+            ""Action""    : [
+                ""sns:GetTopicAttributes"",
+                ""sns:SetTopicAttributes"",
+                ""sns:AddPermission"",
+                ""sns:RemovePermission"",
+                ""sns:DeleteTopic"",
+                ""sns:Subscribe"",
+                ""sns:Publish""
+            ],
+            ""Resource""  : ""{policyDetails.SourceArn}"",
+            ""Condition"" : {{
+                ""StringEquals"" : {{
+                    ""AWS:SourceOwner"" : ""{sourceAccountId}""
+                }}
+            }}
+        }},
+        {{
+            ""Sid"" : ""{Guid.NewGuid().ToString().Replace("-", "")}"",
+            ""Effect"" : ""Allow"",
+            ""Principal"" : {{
+                ""AWS"" : {JsonSerializer.Serialize(policyDetails.AccountIds)}
+            }},
+            ""Action""    : ""sns:Subscribe"",
+            ""Resource""  : ""{policyDetails.SourceArn}""
+        }}
+    ]
+}}";
+        return policyJson;
+    }
+
+    private static string ExtractSourceAccountId(string sourceArn)
+    {
+        //Sns Arn pattern: arn:aws:sns:region:account-id:topic
+        var match = Regex.Match(sourceArn, "(.*?):(.*?):(.*?):(.*?):(.*?):(.*?)", RegexOptions.None, Regex.InfiniteMatchTimeout);
+        return match.Groups[5].Value;
+    }
+}
---
+++ tests/JustSaying.UnitTests/AwsTools/MessageHandling/Sns/Policy/SnsPolicyBuilderTests.cs
@@ -0,0 +1,36 @@
+﻿using JustSaying.AwsTools.MessageHandling;
+using Newtonsoft.Json.Linq;
+
+namespace JustSaying.UnitTests.AwsTools.MessageHandling.Sns.Policy;
+
+public class SnsPolicyBuilderTests
+{
+    [Fact]
+    public void WhenGeneratingPolicy_ForSnsTopic_ThenTheApprovedJsonShouldBeReturned()
+    {
+        // arrange
+        var sourceArn = "arn:aws:sns:ap-southeast-2:123456789012:topic";
+        var snsPolicyDetails = new SnsPolicyDetails
+        {
+            SourceArn = sourceArn
+        };
+
+        // act
+        var policy = SnsPolicyBuilder.BuildPolicyJson(snsPolicyDetails);
+
+        // assert
+        policy.ShouldMatchApproved(c =>
+        {
+            c.SubFolder("Approvals");
+            c.WithScrubber(ScrubSids);
+        });
+    }
+
+    private static string ScrubSids(string iamPolicy)
+    {
+        var json = JObject.Parse(iamPolicy);
+        return iamPolicy
+            .Replace(json["Statement"]![0]!["Sid"]!.ToString(), "<sid1>")
+            .Replace(json["Statement"]![1]!["Sid"]!.ToString(), "<sid2>");
+    }
+}
