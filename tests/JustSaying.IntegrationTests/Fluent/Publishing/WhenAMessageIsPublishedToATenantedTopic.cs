535e0395f85fb29b8209d6d6f4a89f9a64a37f61
martincostello <martin@martincostello.com>
2024-02-01 17:25:52 +0000

    Extend test coverage
    Add more coverage for batch publishing.

--- tests/JustSaying.IntegrationTests/Fluent/Publishing/WhenAMessageIsPublishedToATenantedTopic.cs
+++ tests/JustSaying.IntegrationTests/Fluent/Publishing/WhenAMessageIsPublishedToATenantedTopic.cs
@@ -1,4 +1,5 @@
 using JustSaying.Extensions;
+using JustSaying.Messaging;
 using JustSaying.Messaging.MessageHandling;
 using JustSaying.Models;
 using JustSaying.TestingFramework;
@@ -69,6 +70,71 @@ public class WhenAMessageIsPublishedToATenantedTopic(ITestOutputHelper outputHel
             });
 
         json.ShouldMatchApproved(opt => opt.SubFolder("Approvals"));
+    }
 
+    [AwsFact]
+    public async Task Then_The_Message_Is_Handled_For_Batch()
+    {
+        // Arrange
+        var handler = new InspectableHandler<SimpleMessage>();
+
+        var testId = Guid.NewGuid().ToString("n");
+
+        var topicNameTemplate = "{tenant}-tenanted-topic".TruncateTo(60);
+
+        var services = GivenJustSaying()
+            .ConfigureJustSaying((builder) => builder
+                .Publications(pub => pub.WithTopic<SimpleMessage>(c =>
+                    c.WithTopicName(msg => topicNameTemplate.Replace("{tenant}", msg.Tenant))))
+                .Subscriptions(sub =>
+                    sub.ForTopic<SimpleMessage>(c => c.WithTopicName("uk-tenanted-topic").WithQueueName($"uk-queue-{testId}"))
+                        .ForTopic<SimpleMessage>(c => c.WithTopicName("it-tenanted-topic").WithQueueName($"it-queue-{testId}"))
+                        .ForTopic<SimpleMessage>(c => c.WithTopicName("es-tenanted-topic").WithQueueName($"es-queue-{testId}")))
+            )
+            .AddSingleton<IHandlerAsync<SimpleMessage>>(handler);
+
+        Message CreateMessage(string tenant) => new SimpleMessage()
+        {
+            Content = testId,
+            Tenant = tenant
+        };
+
+        string json = string.Empty;
+
+        await WhenBatchAsync(
+            services,
+            async (publisher, listener, cancellationToken) =>
+            {
+                await listener.StartAsync(cancellationToken);
+                await publisher.StartAsync(cancellationToken);
+
+                // Act
+                await publisher.PublishAsync(
+                    [
+                        CreateMessage("uk"),
+                        CreateMessage("uk"),
+                        CreateMessage("es"),
+                        CreateMessage("es"),
+                        CreateMessage("it"),
+                        CreateMessage("it"),
+                    ],
+                    cancellationToken);
+
+                var publisherJson = JsonConvert.SerializeObject(publisher.Interrogate(), Formatting.Indented);
+
+                json = publisherJson.Replace(UniqueName, "integrationTestQueueName", StringComparison.Ordinal);
+
+                // Assert
+                await Patiently.AssertThatAsync(OutputHelper,
+                    () =>
+                    {
+                        var received = handler.ReceivedMessages;
+                        received.ShouldContain(x => x.Content == testId && x.Tenant == "uk", 2);
+                        received.ShouldContain(x => x.Content == testId && x.Tenant == "it", 2);
+                        received.ShouldContain(x => x.Content == testId && x.Tenant == "es", 2);
+                    });
+            });
+
+        json.ShouldMatchApproved(opt => opt.SubFolder("Approvals"));
     }
 }
---
+++ tests/JustSaying.UnitTests/Messaging/MessagePublisherExtensionsTests.cs
@@ -0,0 +1,60 @@
+using JustSaying.Messaging;
+using JustSaying.Models;
+using NSubstitute;
+
+namespace JustSaying.UnitTests.Messaging;
+
+public static class MessagePublisherExtensionsTests
+{
+    [Fact]
+    public static async Task ArgumentsAreCheckedForNull()
+    {
+        // Arrange
+        var message = Substitute.For<Message>();
+        var messages = new[] { message };
+        var metadata = new PublishMetadata();
+        var batchMetadata = new PublishBatchMetadata();
+
+        // Act and Assert
+        await Assert.ThrowsAsync<ArgumentNullException>("publisher", () => (null as IMessagePublisher).PublishAsync(messages));
+        await Assert.ThrowsAsync<ArgumentNullException>("publisher", () => (null as IMessagePublisher).PublishAsync(messages, CancellationToken.None));
+        await Assert.ThrowsAsync<ArgumentNullException>("publisher", () => (null as IMessagePublisher).PublishAsync(message, metadata));
+        await Assert.ThrowsAsync<ArgumentNullException>("publisher", () => (null as IMessagePublisher).PublishAsync(messages, batchMetadata));
+        await Assert.ThrowsAsync<ArgumentNullException>("publisher", () => (null as IMessagePublisher).PublishAsync(messages, batchMetadata, CancellationToken.None));
+        await Assert.ThrowsAsync<ArgumentNullException>("publisher", () => (null as IMessageBatchPublisher).PublishAsync(messages));
+    }
+
+    [Fact]
+    public static async Task MessagesAreBatchedIfAlsoABatchPublisher()
+    {
+        // Arrange
+        var publisher = Substitute.For<IMessagePublisher, IMessageBatchPublisher>();
+        var messages = new[] { Substitute.For<Message>(), Substitute.For<Message>() };
+        var metadata = new PublishBatchMetadata();
+        var cancellationToken = CancellationToken.None;
+
+        // Act
+        await publisher.PublishAsync(messages, metadata, cancellationToken);
+
+        // Assert
+        await publisher.Received(1).PublishAsync(messages, metadata, cancellationToken);
+        await publisher.Received(0).PublishAsync(Arg.Any<Message>(), Arg.Any<PublishMetadata>(), Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public static async Task MessagesAreSerializedIfNotABatchPublisher()
+    {
+        // Arrange
+        var publisher = Substitute.For<IMessagePublisher>();
+        var messages = new[] { Substitute.For<Message>(), Substitute.For<Message>() };
+        var metadata = new PublishBatchMetadata();
+        var cancellationToken = CancellationToken.None;
+
+        // Act
+        await publisher.PublishAsync(messages, metadata, cancellationToken);
+
+        // Assert
+        await publisher.Received(1).PublishAsync(messages[0], metadata, cancellationToken);
+        await publisher.Received(1).PublishAsync(messages[1], metadata, cancellationToken);
+    }
+}
