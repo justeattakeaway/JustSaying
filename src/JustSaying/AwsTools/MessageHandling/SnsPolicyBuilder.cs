dbb77b3966b37ee7880b0bdb7d30ecd70157835e
Stuart Lang <stuart.b.lang@gmail.com>
2024-02-19 01:40:12 +0000

    Use raw string literals

--- src/JustSaying/AwsTools/MessageHandling/SnsPolicyBuilder.cs
+++ src/JustSaying/AwsTools/MessageHandling/SnsPolicyBuilder.cs
@@ -12,41 +12,43 @@ internal static class SnsPolicyBuilder
             throw new ArgumentException("Must be a valid ARN.", nameof(policyDetails));
         }
         var accountId = arn.AccountId;
-        return $@"{{
-    ""Version"" : ""2012-10-17"",
-    ""Statement"" : [
-        {{
-            ""Sid"" : ""{Guid.NewGuid().ToString().Replace("-", "")}"",
-            ""Effect"" : ""Allow"",
-            ""Principal"" : {{
-                ""AWS"" : ""*""
-            }},
-            ""Action""    : [
-                ""sns:GetTopicAttributes"",
-                ""sns:SetTopicAttributes"",
-                ""sns:AddPermission"",
-                ""sns:RemovePermission"",
-                ""sns:DeleteTopic"",
-                ""sns:Subscribe"",
-                ""sns:Publish""
-            ],
-            ""Resource""  : ""{policyDetails.SourceArn}"",
-            ""Condition"" : {{
-                ""StringEquals"" : {{
-                    ""AWS:SourceOwner"" : ""{accountId}""
-                }}
-            }}
-        }},
-        {{
-            ""Sid"" : ""{Guid.NewGuid().ToString().Replace("-", "")}"",
-            ""Effect"" : ""Allow"",
-            ""Principal"" : {{
-                ""AWS"" : {JsonSerializer.Serialize(policyDetails.AccountIds)}
-            }},
-            ""Action""    : ""sns:Subscribe"",
-            ""Resource""  : ""{policyDetails.SourceArn}""
-        }}
-    ]
-}}";
+        return $$"""
+                 {
+                     "Version" : "2012-10-17",
+                     "Statement" : [
+                         {
+                             "Sid" : "{{Guid.NewGuid().ToString().Replace("-", "")}}",
+                             "Effect" : "Allow",
+                             "Principal" : {
+                                 "AWS" : "*"
+                             },
+                             "Action"    : [
+                                 "sns:GetTopicAttributes",
+                                 "sns:SetTopicAttributes",
+                                 "sns:AddPermission",
+                                 "sns:RemovePermission",
+                                 "sns:DeleteTopic",
+                                 "sns:Subscribe",
+                                 "sns:Publish"
+                             ],
+                             "Resource"  : "{{policyDetails.SourceArn}}",
+                             "Condition" : {
+                                 "StringEquals" : {
+                                     "AWS:SourceOwner" : "{{accountId}}"
+                                 }
+                             }
+                         },
+                         {
+                             "Sid" : "{{Guid.NewGuid().ToString().Replace("-", "")}}",
+                             "Effect" : "Allow",
+                             "Principal" : {
+                                 "AWS" : {{JsonSerializer.Serialize(policyDetails.AccountIds)}}
+                             },
+                             "Action"    : "sns:Subscribe",
+                             "Resource"  : "{{policyDetails.SourceArn}}"
+                         }
+                     ]
+                 }
+                 """;
     }
 }
--- src/JustSaying/AwsTools/MessageHandling/SqsPolicyBuilder.cs
+++ src/JustSaying/AwsTools/MessageHandling/SqsPolicyBuilder.cs
@@ -14,25 +14,27 @@ internal static class SqsPolicyBuilder{
             ? "*"
             : CreateTopicArnWildcard(policyDetails.SourceArn);
 
-        var policyJson = $@"{{
-    ""Version"" : ""2012-10-17"",
-    ""Statement"" : [
-        {{
-            ""Sid"" : ""{sid}"",
-            ""Effect"" : ""Allow"",
-            ""Principal"" : {{
-                ""AWS"" : ""*""
-            }},
-            ""Action""    : ""sqs:SendMessage"",
-            ""Resource""  : ""{resource}"",
-            ""Condition"" : {{
-                ""ArnLike"" : {{
-                    ""aws:SourceArn"" : ""{topicArnWildcard}""
-                }}
-            }}
-        }}
-    ]
-}}";
+        var policyJson = $$"""
+                           {
+                               "Version" : "2012-10-17",
+                               "Statement" : [
+                                   {
+                                       "Sid" : "{{sid}}",
+                                       "Effect" : "Allow",
+                                       "Principal" : {
+                                           "AWS" : "*"
+                                       },
+                                       "Action"    : "sqs:SendMessage",
+                                       "Resource"  : "{{resource}}",
+                                       "Condition" : {
+                                           "ArnLike" : {
+                                               "aws:SourceArn" : "{{topicArnWildcard}}"
+                                           }
+                                       }
+                                   }
+                               ]
+                           }
+                           """;
         return policyJson;
     }
 
