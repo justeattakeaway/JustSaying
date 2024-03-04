97445076b9885c6a443b45b47e17c4ef9066a5d9
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2024-03-04 00:01:40 +0000

    Add test for options extensions

--- src/JustSaying/Extensions/JsonSerializerOptionsExtensions.cs
+++ src/JustSaying/Extensions/JsonSerializerOptionsExtensions.cs
@@ -4,12 +4,12 @@ using System.Text.Json.Serialization.Metadata;
 
 namespace JustSaying.Extensions;
 
-
 internal static class JsonSerializerOptionsExtensions
 {
     public static JsonTypeInfo<T> GetTypeInfo<T>(this JsonSerializerOptions options)
     {
-        // This throws an ArgumentException if type information is not available for a given type.
+        // This throws a NotSupportedException if type information is not available for a given type,
+        // or an ArgumentException if the type is not valid for serialization (void, pointer types, and similar).
         // We don't guard this to allow the default exception to be propagated.
         var typeInfo = options.GetTypeInfo(typeof(T));
         return (JsonTypeInfo<T>)typeInfo;
---
+++ tests/JustSaying.UnitTests/Extensions/JsonSerializerOptionsExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+using JustSaying.Models;
+using JustSaying.Extensions;
+
+namespace JustSaying.UnitTests.Extensions;
+
+public sealed partial class JsonSerializerOptionsExtensionsTests
+{
+    private readonly JsonSerializerOptions _sut = new()
+    {
+        TypeInfoResolver = TestSerializerContext.Default,
+    };
+
+    [Fact]
+    public void GetTypeInfo_ReturnsExpectedValue_WhenOptionsContainTypeInfo()
+    {
+        var result = _sut.GetTypeInfo<TestContainedClass>();
+        result.ShouldBeOfType<JsonTypeInfo<TestContainedClass>>();
+    }
+
+    [Fact]
+    public void GetTypeInfo_ThrowsArgumentException_WhenOptionsDoNotContainTypeInfo()
+    {
+        var testAction = () => _sut.GetTypeInfo<TestNotContainedClass>();
+        testAction.ShouldThrow<NotSupportedException>();
+    }
+
+    public class TestContainedClass : Message;
+
+    public class TestNotContainedClass : Message;
+
+    [JsonSerializable(typeof(TestContainedClass))]
+    public sealed partial class TestSerializerContext : JsonSerializerContext;
+}
