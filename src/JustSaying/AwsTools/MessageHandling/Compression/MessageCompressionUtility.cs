09413c369994f0ff2fc0925e82c4b4990af54218
Stuart Lang <stuart.b.lang@gmail.com>
2024-07-08 14:40:23 +0100

    Add more xml doc comments

--- src/JustSaying/AwsTools/MessageHandling/Compression/MessageCompressionUtility.cs
+++ src/JustSaying/AwsTools/MessageHandling/Compression/MessageCompressionUtility.cs
@@ -4,15 +4,26 @@ using JustSaying.Messaging.Compression;
 
 namespace JustSaying.AwsTools.MessageHandling;
 
+/// <summary>
+/// Provides utility methods for compressing messages based on specified criteria.
+/// </summary>
 internal static class MessageCompressionUtility
 {
+    /// <summary>
+    /// Compresses a message if it meets the specified compression criteria.
+    /// </summary>
+    /// <param name="message">The original message to potentially compress.</param>
+    /// <param name="metadata">Metadata associated with the message.</param>
+    /// <param name="compressionOptions">Options specifying when and how to compress.</param>
+    /// <param name="compressionRegistry">Registry of available compression algorithms.</param>
+    /// <returns>A tuple containing the compressed message (or null if not compressed) and the content encoding used (or null if not compressed).</returns>
     public static (string compressedMessage, string contentEncoding) CompressMessageIfNeeded(string message, PublishMetadata metadata, PublishCompressionOptions compressionOptions, IMessageCompressionRegistry compressionRegistry)
     {
         string contentEncoding = null;
         string compressedMessage = null;
         if (compressionOptions?.CompressionEncoding is { } compressionEncoding && compressionRegistry is not null)
         {
-            var messageSize = CalculateTotalMessageSize(metadata, message);
+            var messageSize = CalculateTotalMessageSize(message, metadata);
             if (messageSize > compressionOptions.MessageLengthThreshold)
             {
                 var compression = GetCompressionAlgorithm(compressionEncoding, compressionRegistry);
@@ -20,11 +31,17 @@ internal static class MessageCompressionUtility
                 contentEncoding = compressionEncoding;
             }
         }
-        
+
         return (compressedMessage, contentEncoding);
     }
 
-    private static int CalculateTotalMessageSize(PublishMetadata metadata, string message)
+    /// <summary>
+    /// Calculates the total size of a message, including its metadata.
+    /// </summary>
+    /// <param name="message">The message content.</param>
+    /// <param name="metadata">Metadata associated with the message.</param>
+    /// <returns>The total size of the message in bytes.</returns>
+    private static int CalculateTotalMessageSize(string message, PublishMetadata metadata)
     {
         var messageSize = Encoding.UTF8.GetByteCount(message);
         foreach (var attribute in metadata.MessageAttributes)
@@ -43,6 +60,13 @@ internal static class MessageCompressionUtility
         return messageSize;
     }
 
+    /// <summary>
+    /// Retrieves the compression algorithm for the specified encoding from the compression registry.
+    /// </summary>
+    /// <param name="compressionEncoding">The encoding of the desired compression algorithm.</param>
+    /// <param name="compressionRegistry">The registry containing available compression algorithms.</param>
+    /// <returns>The compression algorithm corresponding to the specified encoding.</returns>
+    /// <exception cref="PublishException">Thrown when the specified compression encoding is not registered.</exception>
     private static IMessageBodyCompression GetCompressionAlgorithm(string compressionEncoding, IMessageCompressionRegistry compressionRegistry)
     {
         var compression = compressionRegistry.GetCompression(compressionEncoding);
--- src/JustSaying/AwsTools/MessageHandling/Compression/PublishCompressionOptions.cs
+++ src/JustSaying/AwsTools/MessageHandling/Compression/PublishCompressionOptions.cs
@@ -1,7 +1,23 @@
 namespace JustSaying.AwsTools.MessageHandling;
 
+/// <summary>
+/// Represents options for message compression during publishing.
+/// </summary>
 public sealed class PublishCompressionOptions
 {
+    /// <summary>
+    /// Gets or sets the message length threshold in bytes.
+    /// Messages larger than this threshold will be compressed.
+    /// </summary>
+    /// <remarks>
+    /// The default value is 262,144 bytes (256 KB).
+    /// </remarks>
     public int MessageLengthThreshold { get; set; } = 256 * 1024;
+    /// <summary>
+    /// Gets or sets the compression encoding to be used.
+    /// </summary>
+    /// <remarks>
+    /// This should correspond to a registered compression algorithm in the IMessageCompressionRegistry.
+    /// </remarks>
     public string CompressionEncoding { get; set; }
 }
