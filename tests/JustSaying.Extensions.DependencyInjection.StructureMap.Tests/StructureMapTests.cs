e1daeee9adbd408542411d09de534e97a3f20beb
Stuart Lang <stuart.b.lang@gmail.com>
2024-10-11 14:55:51 +0100

    Fix test

--- tests/JustSaying.Extensions.DependencyInjection.StructureMap.Tests/StructureMapTests.cs
+++ tests/JustSaying.Extensions.DependencyInjection.StructureMap.Tests/StructureMapTests.cs
@@ -1,6 +1,7 @@
 using JustSaying.Messaging;
 using JustSaying.Messaging.MessageHandling;
 using JustSaying.TestingFramework;
+using LocalSqsSnsMessaging;
 using Microsoft.Extensions.Logging;
 using Shouldly;
 using StructureMap;
@@ -11,6 +12,7 @@ namespace JustSaying;
 public class WhenUsingStructureMap(ITestOutputHelper outputHelper)
 {
     private ITestOutputHelper OutputHelper { get; } = outputHelper;
+    private InMemoryAwsBus InMemoryAwsBus { get; } = new ();
 
     [AwsFact]
     public async Task Can_Create_Messaging_Bus_Fluently_For_A_Queue()
@@ -32,8 +34,10 @@ public class WhenUsingStructureMap(ITestOutputHelper outputHelper)
                     (builder) =>
                     {
                         builder.Client((options) =>
-                                options.WithBasicCredentials("accessKey", "secretKey")
-                                    .WithServiceUri(TestEnvironment.SimulatorUrl))
+                                    options.WithClientFactory(() => new LocalAwsClientFactory(InMemoryAwsBus))
+                                // options.WithBasicCredentials("accessKey", "secretKey")
+                                //     .WithServiceUri(TestEnvironment.SimulatorUrl)
+                                )
                             .Messaging((options) => options.WithRegion("eu-west-1"))
                             .Publications((options) => options.WithQueue<SimpleMessage>())
                             .Subscriptions((options) => options.ForQueue<SimpleMessage>());
