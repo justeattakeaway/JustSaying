2b4ce1a3c11f389a321bc2b85a49d7199d53dd40
Niall O'Hagan <niall.ohagan@gearset.com>
2025-03-10 14:34:22 +0000

    Additional Integration Tests

---
+++ tests/JustSaying.IntegrationTests/Fluent/AwsTools/WhenANamedFifoQueueIsCreated.cs
@@ -0,0 +1,38 @@
+using JustSaying.AwsTools;
+using JustSaying.AwsTools.MessageHandling;
+using JustSaying.AwsTools.QueueCreation;
+using JustSaying.TestingFramework;
+using Microsoft.Extensions.Logging;
+
+#pragma warning disable 618
+
+namespace JustSaying.IntegrationTests.Fluent.AwsTools;
+
+public class WhenANamedFifoQueueIsCreated(ITestOutputHelper outputHelper) : IntegrationTestBase(outputHelper)
+{
+    [AwsFact]
+    public async Task Then_The_Error_Queue_Is_Created()
+    {
+        // Arrange
+        ILoggerFactory loggerFactory = OutputHelper.ToLoggerFactory();
+        IAwsClientFactory clientFactory = CreateClientFactory();
+
+        var client = clientFactory.GetSqsClient(Region);
+
+        var queue = new SqsQueueByName(
+            Region,
+            UniqueName + ".fifo",
+            true,
+            client,
+            1,
+            loggerFactory);
+
+        // Act
+        await queue.CreateAsync(new SqsBasicConfiguration());
+
+        // Assert
+        await Patiently.AssertThatAsync(OutputHelper,
+            async () => await queue.ErrorQueue.ExistsAsync(CancellationToken.None),
+            40.Seconds());
+    }
+}
