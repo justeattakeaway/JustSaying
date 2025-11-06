59f47bdbed4613945d08794576c107637f3b6c1c
Stuart Lang <stuart.b.lang@gmail.com>
2025-11-06 01:03:32 +0000

    Migrate documentation from GitBook to Docusaurus

---
+++ website/sidebars.js
@@ -0,0 +1,85 @@
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+ */
+
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  // By default, Docusaurus generates a sidebar from the docs folder structure
+  tutorialSidebar: [
+    {
+      type: 'doc',
+      id: 'intro',
+      label: 'Introduction',
+    },
+    'getting-started',
+    'Configuration',
+    {
+      type: 'category',
+      label: 'Messaging Configuration',
+      items: [
+        'messaging-configuration/README',
+        'messaging-configuration/logging',
+        'messaging-configuration/metrics',
+        'messaging-configuration/naming-conventions',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Subscriptions',
+      items: [
+        {
+          type: 'category',
+          label: 'Configuration',
+          items: [
+            'subscriptions/configuration/README',
+            'subscriptions/configuration/fortopic',
+            'subscriptions/configuration/forqueue',
+            'subscriptions/configuration/sqsreadconfiguration',
+            'subscriptions/configuration/handler-registration-and-resolution',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'SubscriptionGroups',
+          items: [
+            'subscriptions/subscriptiongroups/README',
+            'subscriptions/subscriptiongroups/configuration',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Middleware',
+          items: [
+            'subscriptions/middleware/README',
+            'subscriptions/middleware/custom-middleware',
+          ],
+        },
+        'subscriptions/dead-letter-queues',
+        'subscriptions/message-context',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Publications',
+      items: [
+        'publishing/configuration',
+      ],
+    },
+    'how-justsaying-uses-sqs-sns',
+    'interoperability',
+    'aws-iam',
+    'sample-application',
+  ],
+};
+
+module.exports = sidebars;
+
