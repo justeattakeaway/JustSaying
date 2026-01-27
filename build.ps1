ce0d13f6b544a08cdcf50a1d2cd504eba3fc23ad
Jair de Souza Junior <jair.desouza@justeattakeaway.com>
2026-01-27 17:09:00 +0100

    Add the new extension module to the build.ps1 script

--- build.ps1
+++ build.ps1
@@ -23,12 +23,14 @@ $libraryProjects = @(
     (Join-Path $solutionPath "src" "JustSaying.Models" "JustSaying.Models.csproj"),
     (Join-Path $solutionPath "src" "JustSaying.Extensions.Aws" "JustSaying.Extensions.Aws.csproj"),
     (Join-Path $solutionPath "src" "JustSaying.Extensions.DependencyInjection.Microsoft" "JustSaying.Extensions.DependencyInjection.Microsoft.csproj"),
-    (Join-Path $solutionPath "src" "JustSaying.Extensions.DependencyInjection.StructureMap" "JustSaying.Extensions.DependencyInjection.StructureMap.csproj")
+    (Join-Path $solutionPath "src" "JustSaying.Extensions.DependencyInjection.StructureMap" "JustSaying.Extensions.DependencyInjection.StructureMap.csproj"),
+    (Join-Path $solutionPath "src" "JustSaying.Extensions.Kafka" "JustSaying.Extensions.Kafka.csproj")
 )
 
 $testProjects = @(
     (Join-Path $solutionPath "tests" "JustSaying.UnitTests" "JustSaying.UnitTests.csproj"),
-    (Join-Path $solutionPath "tests" "JustSaying.Extensions.DependencyInjection.StructureMap.Tests" "JustSaying.Extensions.DependencyInjection.StructureMap.Tests.csproj")
+    (Join-Path $solutionPath "tests" "JustSaying.Extensions.DependencyInjection.StructureMap.Tests" "JustSaying.Extensions.DependencyInjection.StructureMap.Tests.csproj"),
+    (Join-Path $solutionPath "tests" "JustSaying.Extensions.Kafka.Tests" "JustSaying.Extensions.Kafka.Tests.csproj")
 )
 
 if ($EnableIntegrationTests -eq $true) {
